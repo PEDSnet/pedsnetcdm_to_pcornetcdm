31fcaa0d9c030b419294d2875c41b9765e48aca1
Ritu Khare <writetoritu@gmail.com>
2015-09-08 14:58:14 -0400

    tested version

--- v2 to v3/ETL Scripts/Dispensing_ETL.sql
+++ v2 to v3/ETL Scripts/Dispensing_ETL.sql
@@ -1,23 +1,27 @@
 ﻿
 -- drug exposure --> Dispensing
+-- join with demographic to make sure there are no orphan records 
+-- joins with concept relationship and concept tables are to determine the mapping from RxNorm -> NDC. 
 
 -- assuming that we have changed the datatype of dispensingid to serial
 insert into pcornet_cdm.dispensing(
             patid, prescribingid, 
             dispense_date, ndc, dispense_sup, dispense_amt, raw_ndc)
+with ndc_concept as (select distinct concept_id, concept_code from concept c1 where c1.concept_class_id = '11-digit NDC' and c1.vocabulary_id='NDC' and c1.invalid_reason is null)
 select distinct
 	cast(de.person_id as text) as patid,
 	null as prescribingid, -- null for now
 	de.drug_exposure_start_date as dispense_date,
-	c1.concept_code as ndc,
+	ndc_concept.concept_code as ndc,
 	de.days_supply as dispense_sup,
 	de.quantity as dispense_amt,
-	c1.concept_code as raw_ndc
+	ndc_concept.concept_code as raw_ndc
 from
-	drug_exposure de
-	join pcornet_cdm.demographic d on d.patid = cast(de.person_id as text)
-	join concept_relationship cr on concept_id_1 = de.drug_concept_id and cr.invalid_reason is null and  relationship_id='Mapped from'
-	join concept c1 on cr.concept_id_2 = c1.concept_id AND c1.concept_class_id = '11-digit NDC' and c1.vocabulary_id='NDC' and c1.invalid_reason is null 
+	drug_exposure de  
+	join pcornet_cdm.demographic d on d.patid = cast(de.person_id as text) 
+	join concept_relationship cr on cr.concept_id_1 = de.drug_concept_id  and cr.invalid_reason is null and  relationship_id='Mapped from'  
+	join ndc_concept on cr.concept_id_2= ndc_concept.concept_id
 where
 	de.drug_type_concept_id = '38000175' -- Dispensing only
+
 
