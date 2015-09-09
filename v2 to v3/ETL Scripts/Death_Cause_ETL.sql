424b8bc5b9e48fad91d1991afd348b69281727b5
Ritu Khare <writetoritu@gmail.com>
2015-09-09 10:31:19 -0400

    first draft of death and death cause queries

---
+++ v2 to v3/ETL Scripts/Death_Cause_ETL.sql
@@ -0,0 +1,19 @@
+﻿Insert into pcornet_cdm.death_cause(
+	patid,
+	death_cause, death_cause_code, death_cause_type,
+	death_cause_source, death_cause_confidence	
+)
+select 
+	person_id as patid,
+	c1.concept_code as death_cause, --- no more than 8 characters
+	coalesce(m1.target_concept,'OT') as death_cause_code,
+	'NI' as death_cause_type, -- cannot be NULL
+	'L' as death_cause_source,
+	null as death_cause_confidence 
+From
+	death de
+	join pcornet_cdm.demographic d on cast(de.person_id as text) = d.patid
+	join concept c1 on cause_concept_id = c1.concept_id
+	left join pcornet_cdm.cz_omop_pcornet_concept_map m1 on cast(cause_concept_id as text) = source_concept_id 
+		AND m1.source_concept_class='death cause code' 
+	
---
+++ v2 to v3/ETL Scripts/Death_ETL.sql
@@ -0,0 +1,16 @@
+﻿Insert into pcornet_cdm.death(
+	patid, death_date, death_date_impute,
+	death_source, death_match_confidence	
+)
+Select 
+	de.person_id as patid,
+	de.death_date as death_date,
+	'N' as death_impute, -- No for now, where to store this information in PEDSnet CDM?
+	'L' as death_source, -- death data from the EHR
+	null as death_match_confidence -- null since records are not from an external source. Possibly record linkage use case
+From
+	death de
+	join pcornet_cdm.demographic d on d.patid = cast(de.person_id as text)
+Where 
+	de.death_type_concept_id  = 38003569;
+	 
