0dcb5bf6993cd89304860e0d0188d80e3fb8a50e
Ritu Khare <writetoritu@gmail.com>
2015-09-23 10:41:37 -0400

    adding decisions from 40, 41, 43

--- v2 to v3/ETL Scripts/Lab_Result_CM_ETL.sql
+++ v2 to v3/ETL Scripts/Lab_Result_CM_ETL.sql
@@ -1,5 +1,4 @@
-﻿
-insert into pcornet_cdm.lab_result_cm (
+﻿insert into pcornet_cdm.lab_result_cm (
 	lab_result_cm_id,
 	patid, encounterid,
 	lab_name, specimen_source,
@@ -22,22 +21,22 @@ select
 	m2.target_concept as specimen_source,
 	c1.concept_code as lab_loinc,
 	null as priority,  -- null for now
-	'UN' as result_loc, -- making this 'UN' for now - work in progress to edit the measurement table to include this. 	
+	'UN' as result_loc, -- making this 'UN' for now - work in progress to edit the measurement table to include this. 
 	null as lab_px, -- null for now
 	null as lab_px_type,
-	null as lab_order_date, -- not available in PEDSnet CDM
-	m.measurement_date as specimen_date,
+	m.measurement_date as lab_order_date, -- default:  populate all dates using the measurement date (only date that we have at the moment)- until new conventions
+	m.measurement_date as specimen_date,  -- default:  populate all dates using the measurement date (only date that we have at the moment)- until new conventions
 	date_part('hour',m.measurement_time)||':'||date_part('minute',m.measurement_time) as specimen_time, -- HH:MI format 
-	m.measurement_date as result_date, -- is that correct?
+	m.measurement_date as result_date, -- default:  populate all dates using the measurement date (only date that we have at the moment) - until new conventions
 	null as result_time,
-	'NI' as result_qual, -- derived field?
+	'NI' as result_qual, -- Assert NI for now --- until new conventions evolve
 	m.value_as_number as result_num,
 	m3.target_concept as result_modifier,
 	m4.target_concept as result_unit,
-	m.range_low::text as norm_range_low, -- TODO: incoporate the solution for non-numeric range values
-	null as norm_modifier_low, -- No place for this in PEDSNet CDM. Link to observation maybe?
-	m.range_high::text as norm_range_high, 
-	null as norm_modifier_high, -- same as above
+	m.range_low::text as norm_range_low, -- waiting on new conventions 2.1
+	null as norm_modifier_low, -- -- waiting on new conventions 2.1
+	m.range_high::text as norm_range_high, -- waiting on new conventions 2.1
+	null as norm_modifier_high, -- -- waiting on new conventions 2.1
 	null as abn_ind, -- null for now
 	c1.concept_name as raw_lab_name,
 	m.measurement_id as raw_lab_code,
@@ -56,5 +55,4 @@ from
 	left join pcornet_cdm.cz_omop_pcornet_concept_map m1 on c1.concept_code = m1.source_concept_id and m1.source_concept_class = 'Lab name'
 	left join pcornet_cdm.cz_omop_pcornet_concept_map m2 on c1.concept_code = m2.source_concept_id and m2.source_concept_class = 'Specimen source'
 	left join pcornet_cdm.cz_omop_pcornet_concept_map m3 on cast(m.operator_concept_id as text) = m3.source_concept_id and m3.source_concept_class = 'Result modifier'
-	left join pcornet_cdm.cz_omop_pcornet_concept_map m4 on cast(m.unit_concept_id as text)= m4.source_concept_id and m4.source_concept_class = 'Unit'
-
+	
