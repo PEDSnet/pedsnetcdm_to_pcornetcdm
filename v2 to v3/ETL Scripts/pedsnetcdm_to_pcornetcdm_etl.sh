1f9ef6d8188ae394feb30eef9bb997f0dc6cfb0c
LeMar Davidson <davidsonl2@email.chop.edu>
2015-10-08 10:18:13 -0400

    Adding updated encounter script

---
+++ v2 to v3/ETL Scripts/pedsnetcdm_to_pcornetcdm_etl.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export SCRIPTNAME="pcornetcdm_ddl.sql"      && echo "Beginning Execution of $SCRIPTNAME" > log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="cz_omop_pcornet_concept_map_ddl.sql"      && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="copy_mappings.sql"       && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Demographic_ETL.sql"     && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Enrollment_ETL.sql"      && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Death_ETL.sql"           && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Death_Cause_ETL.sql"     && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Encounter_ETL.sql"       && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Condition_ETL.sql"       && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Diagnosis_ETL.sql"       && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Procedure_ETL.sql"       && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Dispensing_ETL.sql"      && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Prescribing_ETL.sql"     && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Vital_ETL.sql"           && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
+
+export SCRIPTNAME="Lab_Result_CM_ETL.sql"   && echo "Beginning Execution of $SCRIPTNAME" >> log/"$DATABASE"_etl.log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"   nohup time psql --host="$DBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" < "$SCRIPTNAME" >> log/"$DATABASE"_etl.log
