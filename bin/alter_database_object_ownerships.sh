f5c9f88c5f694600cfcc9475361606df60cec149
LeMar Davidson <davidsonl2@email.chop.edu>
2015-10-20 18:23:36 -0400

    Add Database name to log output

--- bin/alter_database_object_ownerships.sh
+++ bin/alter_database_object_ownerships.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2"  --dbname="{$DATABASE}" -c "alter database pedsnet_nationwide owner to pcor_et_user;"
-PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2"  --dbname="{$DATABASE}" -c "alter schema pedsnet_nationwide owner to pcor_et_user;"
-PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2"  --dbname="{$DATABASE}" -c "alter schema pcornet_cdm owner to pcor_et_user;"
-PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2"  --dbname="{$DATABASE}" -c "alter schema public owner to pcor_et_user;"
-PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2"  --dbname="{$DATABASE}" -c "alter table public.measurement owner to pcor_et_user;"
-PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2"  --dbname="{$DATABASE}" -c "set role pcor_et_user;"
-PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2"  --dbname="{$DATABASE}" -c "select * from public.measurement;"
-PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2"  --dbname="{$DATABASE}" -c "select * from public.measurement limit 1;"
+SCHEMAOWNER=dcc_owner
 
-for tbl in `PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2" --dbname="${DATABASE}" -qAt -c "select tablename from pg_tables where schemaname = 'public';"` ; do  PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2" --dbname="${DATABASE}" -c "alter table public.\"$tbl\" owner to pcor_et_user" ; done
+HOSTNAME=$LOADDBHOSTNAME
 
-for tbl in `PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2" --dbname="${DATABASE}" -qAt -c "select tablename from pg_tables where schemaname = 'pcornet_cdm';"` ; do  PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2" --dbname="${DATABASE}" -c "alter table pcornet_cdm.\"$tbl\" owner to pcor_et_user" ; done
+HOSTNAME=localhost
 
-for tbl in `PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2" --dbname="${DATABASE}" -qAt -c "select tablename from pg_tables where schemaname = 'pedsnet_cdm';"` ; do  PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="reslnpedsndb02.research.chop.edu" --user="davidsonl2" --dbname="${DATABASE}" -c "alter table pedsnet_cdm.\"$tbl\" owner to pcor_et_user" ; done
+PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" -c "alter database pedsnet_nationwide owner to $SCHEMAOWNER;"
+PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="$ETL_DATABASE_USER"  --dbname="{$DATABASE}" -c "alter schema pedsnet_nationwide owner to $SCHEMAOWNER;"
+PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" -c "alter schema pcornet_cdm owner to $SCHEMAOWNER;"
+PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="$ETL_DATABASE_USER"  --dbname="{$DATABASE}" -c "alter schema public owner to $SCHEMAOWNER;"
+PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="$ETL_DATABASE_USER"  --dbname="{$DATABASE}" -c "alter table public.measurement owner to $SCHEMAOWNER;"
+PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="$ETL_DATABASE_USER"  --dbname="{$DATABASE}" -c "set role $SCHEMAOWNER;"
+PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="$ETL_DATABASE_USER"  --dbname="{$DATABASE}" -c "select * from public.measurement;"
+PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="$ETL_DATABASE_USER"  --dbname="{$DATABASE}" -c "select * from public.measurement limit 1;"
+
+for tbl in `PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="davidsonl2" --dbname="${DATABASE}" -qAt -c "select tablename from pg_tables where schemaname = 'public';"` ; do  PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$LOADDBHOSTNAME" --user="davidsonl2" --dbname="${DATABASE}" -c "alter table public.\"$tbl\" owner to $SCHEMAOWNER" ; done
+
+for tbl in `PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="davidsonl2" --dbname="${DATABASE}" -qAt -c "select tablename from pg_tables where schemaname = 'pcornet_cdm';"` ; do  PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$LOADDBHOSTNAME" --user="davidsonl2" --dbname="${DATABASE}" -c "alter table pcornet_cdm.\"$tbl\" owner to $SCHEMAOWNER" ; done
+
+for tbl in `PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$HOSTNAME" --user="davidsonl2" --dbname="${DATABASE}" -qAt -c "select tablename from pg_tables where schemaname = 'pedsnet_cdm';"` ; do  PGPASSWORD="$ETL_DATABASE_USER_PASSWORD" psql --host="$LOADDBHOSTNAME" --user="davidsonl2" --dbname="${DATABASE}" -c "alter table pedsnet_cdm.\"$tbl\" owner to $SCHEMAOWNER" ; done
+
+
+Load (Production)
+
+psql  --dbname="$DATABASE" -c "alter schema pcornet_cdm owner to $SCHEMAOWNER;"
--- v2 to v3/ETL Scripts/pcornetcdm_to_pcornetcdm_load.sh
+++ v2 to v3/ETL Scripts/pcornetcdm_to_pcornetcdm_load.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Beginning Load of Datbase ${DATABASE}" > log/"$LOADFILENAME".log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"  psql --host="$LOADDBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE"   < dumps/"${DUMPFILENAME}"
+echo "Beginning Load of Datbase ${DATABASE}" > log/"$LOGFILENAME".log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"  psql --host="$LOADDBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE"   < dumps/"${DUMPFILENAME}"
+
+
+echo "Beginning Load of Datbase ${DATABASE}" > log/$LOGFILENAME && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"  psql --host="$LOADDBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE"   < dumps/${DUMPFILENAME}
