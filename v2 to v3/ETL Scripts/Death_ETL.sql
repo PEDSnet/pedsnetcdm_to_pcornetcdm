dfe1789d88855161c3419ccdfab8db19d5be4345
writetoritu <kharer@email.chop.edu>
2016-11-01 12:21:12 -0400

    selecting one death record in case of multiple causes

--- v2 to v3/ETL Scripts/Death_ETL.sql
+++ v2 to v3/ETL Scripts/Death_ETL.sql
@@ -1,3 +1,4 @@
+﻿
 Insert into dcc_pcornet.death(
 	patid, death_date, death_date_impute,
 	death_source, death_match_confidence, siteid
@@ -8,10 +9,13 @@ Select
 	coalesce(m1.target_concept,'OT') as death_impute, 
 	'L' as death_source, --  default for now until new conventions
 	null as death_match_confidence, --  we do not capture it dicretely in the EHRs 
-	site_id as siteid
+	min(site_id) as siteid -- retrieve one record in case of multiple death causes
 From
 	dcc_pedsnet.death de
 	join dcc_pcornet.demographic d on d.patid = cast(de.person_id as text)
-	left join dcc_pcornet.cz_omop_pcornet_concept_map m1 on m1.source_concept_class='Death date impute' and cast(de.death_impute_concept_id as text) = m1.source_concept_id
+	left join dcc_pcornet.cz_omop_pcornet_concept_map m1 on m1.source_concept_class='Death date impute' and 
+	cast(de.death_impute_concept_id as text) = m1.source_concept_id
 Where 
-	de.death_type_concept_id  = 38003569;
+	de.death_type_concept_id  = 38003569
+group by de.person_id , de.death_date, coalesce(m1.target_concept,'OT'); 
+	
