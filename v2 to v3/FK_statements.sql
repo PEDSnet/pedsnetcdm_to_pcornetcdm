1bb87208602692d97ee27b124475ddab4ee26927
writetoritu <kharer@email.chop.edu>
2017-01-10 21:46:16 -0500

    adding FK and indexes

---
+++ v2 to v3/FK_statements.sql
@@ -0,0 +1,31 @@
+﻿ALTER TABLE dcc_pcornet.encounter ADD CONSTRAINT enc_fk FOREIGN KEY ( patid ) REFERENCES dcc_pcornet.demographic (patid); 
+
+ALTER TABLE dcc_pcornet.condition ADD CONSTRAINT cond_fk_1 FOREIGN KEY ( patid ) REFERENCES dcc_pcornet.demographic (patid); 
+
+-- following was done
+ALTER TABLE dcc_pcornet.condition ADD CONSTRAINT cond_fk_2 FOREIGN KEY ( encounterid ) REFERENCES dcc_pcornet.encounter (encounterid); 
+
+-- following was done
+ALTER TABLE dcc_pcornet.diagnosis ADD CONSTRAINT diag_fk_1 FOREIGN KEY ( patid ) REFERENCES dcc_pcornet.demographic (patid); 
+
+ALTER TABLE dcc_pcornet.diagnosis ADD CONSTRAINT diag_fk_2 FOREIGN KEY ( encounterid ) REFERENCES dcc_pcornet.encounter (encounterid); 
+
+
+ALTER TABLE dcc_pcornet.procedures ADD CONSTRAINT proc_fk_1 FOREIGN KEY ( patid ) REFERENCES dcc_pcornet.demographic (patid); 
+
+ALTER TABLE dcc_pcornet.procedures ADD CONSTRAINT proc_fk_2 FOREIGN KEY ( encounterid ) REFERENCES dcc_pcornet.encounter (encounterid); 
+
+ALTER TABLE dcc_pcornet.dispensing ADD CONSTRAINT disp_fk FOREIGN KEY ( patid ) REFERENCES dcc_pcornet.demographic (patid); 
+
+ALTER TABLE dcc_pcornet.prescribing ADD CONSTRAINT pres_fk_1 FOREIGN KEY ( patid ) REFERENCES dcc_pcornet.demographic (patid); 
+
+ALTER TABLE dcc_pcornet.prescribing ADD CONSTRAINT pres_fk_2 FOREIGN KEY ( encounterid ) REFERENCES dcc_pcornet.encounter (encounterid); 
+
+ALTER TABLE dcc_pcornet.vital ADD CONSTRAINT vital_fk_1 FOREIGN KEY ( patid ) REFERENCES dcc_pcornet.demographic (patid); 
+
+ALTER TABLE dcc_pcornet.vital ADD CONSTRAINT vital_fk_2 FOREIGN KEY ( encounterid ) REFERENCES dcc_pcornet.encounter (encounterid); 
+
+
+ALTER TABLE dcc_pcornet.lab_result_cm ADD CONSTRAINT lab_fk_1 FOREIGN KEY ( patid ) REFERENCES dcc_pcornet.demographic (patid); 
+
+ALTER TABLE dcc_pcornet.lab_result_cm ADD CONSTRAINT lab_fk_2 FOREIGN KEY ( encounterid ) REFERENCES dcc_pcornet.encounter (encounterid); 
