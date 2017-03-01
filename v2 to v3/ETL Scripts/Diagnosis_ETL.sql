48d6e36c91b21fe08fb3a870a1765008b35b06d7
writetoritu <kharer@email.chop.edu>
2017-03-01 14:56:28 -0500

    reordering vocabularies

--- v2 to v3/ETL Scripts/Diagnosis_ETL.sql
+++ v2 to v3/ETL Scripts/Diagnosis_ETL.sql
@@ -2,6 +2,8 @@
 -- Changes from previous version:
 ---- Drive dx_source from Observation.value_as_concept_id
 ---- Populate Pdx,raw_pdx, raw_dx_source
+-- condition_occurrence --> Diagnosis
+delete from dcc_pcornet.diagnosis
 
 insert into dcc_pcornet.diagnosis(
             diagnosisid,patid, encounterid, enc_type, admit_date, providerid, dx, dx_type, 
@@ -13,18 +15,18 @@ select distinct
 	enc.enc_type,
 	enc.admit_date,
 	enc.providerid,
-	case when condition_concept_id>0 then c1.concept_code
-			else  case when site in('chop','nemours','stlouis') then split_part(condition_source_value,'|',2) 
-			            else case when site = ('colorado') then split_part(condition_source_value,'|',1) 
-					else  condition_source_value end end
-		end as dx,
-	case when condition_concept_id>0 then  'SM'
-		else case 
-			when c2.vocabulary_id = 'ICD9CM'  then '09' 
-			else 
-			case when  c2.vocabulary_id = 'ICD10' then '10' else 'OT' 
-			end
-		     end 
+	-- look for ICDs, followed by SNOMED, following by others
+	case when c3.vocabulary_id in ('ICD9CM', 'ICD10','ICD10CM') 
+		then split_part(condition_source_value,'|',2) 
+		else case when co.condition_concept_id>0
+		 then c2.concept_code 
+		 else split_part(condition_source_value,'|',2)  end end 
+			           as dx,
+	case when c3.vocabulary_id = 'ICD9CM'  then '09' 
+		else 
+		case when  c3.vocabulary_id in ('ICD10','ICD10CM') then '10' else 
+		case when co.condition_concept_id> 0 then 'SM' else 'OT' end  
+		end 
 	end as dx_type,
 	coalesce(m1.target_concept,'OT') as dx_source,
 	case when enc_type in ('IP','IS') then coalesce(m2.target_concept,'OT') else case when enc_type in ('ED','AV','OA') then 'X' else NULL end end as pdx,
@@ -32,12 +34,11 @@ select distinct
 	case when co.condition_source_concept_id = '44814649' then 'OT' else c3.vocabulary_id end as raw_dx_type,
         c4.concept_name as raw_dx_source,	
 	case when co.condition_type_concept_id IN ('38000199','38000201') then c4.concept_name else NULL end as raw_pdx,
-	site as site
+	co.site as site
 from
 	dcc_pedsnet.condition_occurrence co
+	join vocabulary.concept c2 on co.condition_concept_id = c2.concept_id
 	join dcc_pcornet.encounter enc on cast(co.visit_occurrence_id as text)=enc.encounterid
-	join vocabulary.concept c1 on co.condition_concept_id = c1.concept_id  
-	join vocabulary.concept c2 on co.condition_source_concept_id = c2.concept_id -- Join or LEFT JOIN — determine the source vocabulary
 	left join dcc_pcornet.cz_omop_pcornet_concept_map m1 on m1.source_concept_class='dx_source' and cast(co.condition_type_concept_id as text) = m1.source_concept_id
 	left join dcc_pcornet.cz_omop_pcornet_concept_map m2 on  cast(co.condition_type_concept_id as text) = m2.source_concept_id  and m2.source_concept_class='pdx'
 	left join vocabulary.concept c3 on co.condition_source_concept_id = c3.concept_id
