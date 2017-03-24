0736aceedcef8923046ab4c393e3c75a1689d916
piglet25 <hagues@f07960202ee2.chop.edu>
2017-03-24 17:49:17 -0400

    CHOP start2001 Death_cause
    Updated Death_cause script to use CHOP PEDSnet data and
    restrict to 2001 or later.

---
+++ start2001/ETL Scripts/chop/Death_Cause_ETL.sql
@@ -0,0 +1,24 @@
+Insert into chop_start2001_pcornet.death_cause(
+	patid,
+	death_cause, death_cause_code, death_cause_type,
+	death_cause_source, death_cause_confidence, site
+)
+select 
+	person_id as patid,
+	left(cause_source_value,8) as death_cause,
+	coalesce(m1.target_concept,'OT') as death_cause_code,
+	'NI' as death_cause_type, 
+	'L' as death_cause_source,
+	null as death_cause_confidence, -- not dicretely captured in the EHRs
+	min(de.site) as site
+From
+	chop_pedsnet.death de
+	join chop_start2001_pcornet.demographic d on cast(de.person_id as text) = d.patid
+	join vocabulary.concept on cause_source_concept_id = concept_id
+	left join chop_start2001_pcornet.cz_omop_pcornet_concept_map m1 on cast(vocabulary_id as text) = source_concept_id 
+	AND m1.source_concept_class='death cause code' 
+where 
+	cause_source_value is not null and
+	cause_source_concept_id<>44814650 and
+	de.person_id IN (select person_id from chop_start2001_pcornet.person_visit_start2001)
+group by person_id, left(cause_source_value,8), coalesce(m1.target_concept,'OT')  ; 
