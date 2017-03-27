a6f841c41fe3ecc4322800c4448895fcc503466c
piglet25 <hagues@f07960202ee2.chop.edu>
2017-03-27 18:15:10 -0400

    updated chop and dcc to start2001
    finished chop2001 and all dcct2001.

---
+++ start2001/ETL Scripts/dcc/Lab_Result_CM_ETL.sql
@@ -0,0 +1,67 @@
+-- more changes likely to be made based on decisions in data models #203 and #204
+insert into dcc_start2001_pcornet.lab_result_cm (
+	lab_result_cm_id,
+	patid, encounterid,
+	lab_name, specimen_source,
+	lab_loinc, priority, result_loc,
+	lab_px, lab_px_type,
+	lab_order_date, 
+	specimen_date, specimen_time,
+	result_date, result_time, result_qual, result_num, result_modifier, result_unit,
+	norm_range_low, norm_modifier_low,
+	norm_range_high, norm_modifier_high,
+	abn_ind,
+	raw_lab_name, raw_lab_code, raw_panel, raw_result, raw_unit, raw_order_dept, raw_facility_code, site
+)
+
+select 
+	m.measurement_id as lab_result_cm_id,
+	m.person_id as patid,
+	e.encounterid encounterid,
+	m1.target_concept as lab_name,
+	--m2.target_concept as specimen_source,
+	'BLOOD' as specimen_source, -- defaulting to blood until we have a good solution for sites to figure out how to infer specimen source from labs in the EHR data
+	c1.concept_code as lab_loinc,
+	m7.target_concept as priority,  -- null for now bring discussed in Data Models #203
+	case when measurement_source_value like 'POC%' then 'P' else 'L' end as result_loc, -- using logic to distinguish between POC and L for now - work in progress to explicitly include this in measurement table
+	null as lab_px, -- null as discussed in Data Models #204
+	null as lab_px_type, -- null as discussed in Data Models #204
+	m.measurement_order_date as lab_order_date,
+	m.measurement_date as specimen_date,  
+	date_part('hour',m.measurement_time)||':'||date_part('minute',m.measurement_time) as specimen_time, -- HH:MI format 
+	coalesce(measurement_result_date, measurement_date) as result_date, -- temp fix: use measurement_date is result date is unavailable 
+	date_part('hour',m.measurement_result_time)||':'||date_part('minute',m.measurement_result_time) as result_time,
+	'NI' as result_qual, -- Assert NI for now --- until new conventions evolve
+	m.value_as_number as result_num,
+	m3.target_concept as result_modifier,
+	m4.target_concept as result_unit,
+	left(m.range_low::text,10) as norm_range_low, 
+	m5.target_concept as norm_modifier_low, 
+	left(m.range_high::text,10) as norm_range_high,
+	m6.target_concept as norm_modifier_high, 
+	null as abn_ind, -- null for now until new conventions evolve
+	c1.concept_name as raw_lab_name,
+	m.measurement_id as raw_lab_code,
+	null as raw_panel,
+	c2.concept_name || m.value_as_number::text as raw_result,
+	unit_source_value as raw_unit,
+	null as raw_order_dept,
+	null as raw_facility_code,
+	site as site 
+	
+from	 
+	dcc_pedsnet.measurement m
+	join dcc_start2001_pcornet.demographic d on cast(m.person_id as text)= d.patid
+	join dcc_start2001_pcornet.encounter e on cast(m.visit_occurrence_id as text) = e.encounterid
+	join vocabulary.concept c1 on m.measurement_concept_id = c1.concept_id and c1.vocabulary_id = 'LOINC'
+	join vocabulary.concept c2 on m.operator_concept_id = c2.concept_id and c2.domain_id = 'Meas Value Operator'
+	join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m1 on c1.concept_code = m1.source_concept_id and m1.source_concept_class = 'Lab name'
+	--left join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m2 on c1.concept_code = m2.source_concept_id and m2.source_concept_class = 'Specimen source'
+	left join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m3 on cast(m.operator_concept_id as text) = m3.source_concept_id and m3.source_concept_class = 'Result modifier'
+	left join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m4 on cast(m.unit_concept_id as text)= m4.source_concept_id and m4.source_concept_class = 'Unit'
+	left join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m5 on cast(m.range_low_operator_concept_id as text)= m5.source_concept_id and m5.source_concept_class = 'Result modifier'
+	left join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m6 on cast(m.range_high_operator_concept_id as text)= m6.source_concept_id and m6.source_concept_class = 'Result modifier'
+	left join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m7 on cast(m.priority_concept_id as text)= m7.source_concept_id and m7.source_concept_class = 'Lab priority'
+where
+	m.visit_occurrence_id IN (select visit_id from dcc_start2001_pcornet.person_visit_start2001)
+
---
+++ start2001/ETL Scripts/dcc/Vital_ETL.sql
@@ -0,0 +1,87 @@
+---------- Vital query 
+-- observation --> vital 
+-- Changes from previous version:
+---- Change source table from observation to measurement
+---- Populate vital_source, raw vital source, raw diastolic and raw systolic
+---- Use fact_relationship to tie diastolic BP and systolic PB
+--- Not asked will be skipped and will be incorporated in the next ETL cycle since it is a meaningful use concept.(v2.1)
+--- The query is agnostic to the concept id of the relationship.
+-- We use a concatenated PK (based on ht, wt, dia, sys, bmi measurement ids
+
+drop sequence if exists sq_vitalid;
+create sequence sq_vitalid start 1;
+
+insert into dcc_start2001_pcornet.vital(
+            vitalid, patid, encounterid, measure_date, measure_time, vital_source, 
+            ht, wt, diastolic, systolic, original_bmi, bp_position, 
+	    tobacco, tobacco_type, smoking
+            ,raw_diastolic, raw_systolic, raw_bp_position,site)
+ WITH
+ms as (select  person_id, min(site) as site,visit_occurrence_id,measurement_date, measurement_time  
+		from dcc_pedsnet.measurement where measurement_concept_id IN ('3023540','3013762','3034703','3019962','3013940','3012888','3018586','3035856','3009395','3004249','3038553')
+		group by person_id, visit_occurrence_id,measurement_date, measurement_time  
+		),
+ms_ht as (select distinct measurement_id, visit_occurrence_id, measurement_date, measurement_time, value_as_number  from dcc_pedsnet.measurement where measurement_concept_id = '3023540'),
+ms_wt as (select distinct measurement_id,visit_occurrence_id, measurement_date, measurement_time, value_as_number  from dcc_pedsnet.measurement where measurement_concept_id = '3013762'),
+ms_bmi as (select distinct measurement_id,visit_occurrence_id, measurement_date, measurement_time, value_as_number  from dcc_pedsnet.measurement where measurement_concept_id = '3038553'),
+ms_sys as (select distinct measurement_id, visit_occurrence_id, measurement_date, measurement_time, value_as_number, value_as_concept_id, measurement_concept_id,measurement_source_value from dcc_pedsnet.measurement where measurement_concept_id in ('3018586','3035856','3009395','3004249')),
+ms_dia as (select distinct measurement_id, visit_occurrence_id, measurement_date, measurement_time, measurement_type_concept_id, value_as_number, measurement_source_value from dcc_pedsnet.measurement where measurement_concept_id in ('3034703','3019962','3013940','3012888')),
+ms_vs as (select distinct measurement_id, visit_occurrence_id, measurement_date, measurement_time, measurement_type_concept_id from dcc_pedsnet.measurement where measurement_type_concept_id IN ('44814721')),
+ob_tobacco as ( select distinct observation_id, visit_occurrence_id, observation_date, observation_time,coalesce(m1.target_concept,'OT') as tobacco 
+	from dcc_pedsnet.observation o1 left join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m1 on cast(o1.value_as_concept_id as text) = m1.source_concept_id
+	where observation_concept_id IN ('4005823')),
+ob_tobacco_type as (select distinct observation_id, visit_occurrence_id, observation_date, observation_time, coalesce(m2.target_concept,'OT') as tobacco_type 
+	from dcc_pedsnet.observation o1 
+	left join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m2 on cast(o1.value_as_concept_id as text) = m2.source_concept_id
+	where observation_concept_id IN ('4219336')),
+ob_smoking as (select distinct observation_id, visit_occurrence_id, observation_date, observation_time, coalesce(m3.target_concept,'OT') as smoking 
+	from dcc_pedsnet.observation o1 left join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m3 on cast(o1.value_as_concept_id as text)= m3.source_concept_id
+	where observation_concept_id IN ('4275495')),
+ob_tobacco_data as (select ob_tobacco.visit_occurrence_id, ob_tobacco.observation_date, ob_tobacco.observation_time, ob_tobacco.tobacco, ob_tobacco_type.tobacco_type, ob_smoking.smoking  
+	from ob_tobacco 
+	left join dcc_pedsnet.fact_relationship fr2 on ob_tobacco.observation_id = fr2.fact_id_1 
+	join ob_tobacco_type on fr2.fact_id_2 = ob_tobacco_type.observation_id
+	left join dcc_pedsnet.fact_relationship fr3 on ob_tobacco.observation_id = fr3.fact_id_1 
+	join ob_smoking on fr3.fact_id_2 = ob_smoking.observation_id)
+SELECT distinct
+nextval('sq_vitalid') as vitalid,
+cast(ms.person_id as text) as patid,
+cast(ms.visit_occurrence_id as text) as encounterid,
+cast(cast(date_part('year', ms.measurement_date) as text)||'-'||lpad(cast(date_part('month', ms.measurement_date) as text),2,'0')||'-'||lpad(cast(date_part('day', ms.measurement_date) as text),2,'0') as date) 
+     as measure_date,
+lpad(cast(date_part('hour', ms.measurement_time) as text),2,'0')||':'||lpad(cast(date_part('minute', ms.measurement_time) as text),2,'0') as measure_time,
+'HC' as vital_source, -- defaulting to 'HC' 
+-- In the meanwhile, we will ask the sites whether they can differentiate between HC and HD and will make any applicable changes to PEDSnet conventions 2.1
+    (ms_ht.value_as_number*0.393701) as ht, -- cm to inch conversion
+    (ms_wt.value_as_number*2.20462) as wt, -- kg to pound conversion
+ms_dia.value_as_number as diastolic,
+ms_sys.value_as_number as systolic,
+ms_bmi.value_as_number as original_bmi,
+coalesce(m.target_concept,'OT') as bp_position,
+ob_tobacco_data.tobacco as tobacco,
+ob_tobacco_data.tobacco_type as tobacco_type,
+ob_tobacco_data.smoking as smoking,
+ms_dia.measurement_source_value as raw_diastolic,
+ms_sys.measurement_source_value as raw_systolic,
+null as raw_bp_position, -- Charlie saying not to capture this even though some sties may have store this explicitly - too much effort for populating a raw field 
+ms.site as site
+FROM 
+ms
+left join ms_ht on ms.visit_occurrence_id = ms_ht.visit_occurrence_id 
+and ms.measurement_time = ms_ht.measurement_time 
+left join ms_wt on ms.visit_occurrence_id = ms_wt.visit_occurrence_id 
+and ms.measurement_time = ms_wt.measurement_time 
+left join ms_vs on ms. visit_occurrence_id = ms_vs.visit_occurrence_id 
+and ms.measurement_time = ms_vs.measurement_time
+left join ms_sys on ms.visit_occurrence_id = ms_sys.visit_occurrence_id 
+and ms.measurement_time = ms_sys.measurement_time
+left join dcc_pedsnet.fact_relationship fr1 on fr1.fact_id_1 = ms_sys.measurement_id AND fr1.domain_concept_id_1=21 AND fr1.domain_concept_id_2=21
+left join ms_dia on ms.visit_occurrence_id = ms_dia.visit_occurrence_id 
+and ms_dia.measurement_id = fr1.fact_id_2
+left join ms_bmi on ms.visit_occurrence_id = ms_bmi.visit_occurrence_id 
+and ms.measurement_time = ms_bmi.measurement_time 
+left join dcc_start2001_pcornet.cz_omop_pcornet_concept_map m on cast(ms_sys.measurement_concept_id as text) = m.source_concept_id AND m.source_concept_class='BP Position'
+left join ob_tobacco_data on ms.visit_occurrence_id = ob_tobacco_data.visit_occurrence_id 
+and ms.measurement_time = ob_tobacco_data.observation_time
+where coalesce(ms_ht.value_as_number, ms_wt.value_as_number, ms_dia.value_as_number, ms_sys.value_as_number, ms_bmi.value_as_number) is not null and
+ms.visit_occurrence_id IN (select visit_id from dcc_start2001_pcornet.person_visit_start2001)
