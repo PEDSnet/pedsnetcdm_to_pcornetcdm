df892cf7ba1f3a56e58e09ad09edb678a02645fe
piglet25 <hagues@f07960202ee2.chop.edu>
2017-04-05 14:47:42 -0400

    added cz_omop_pcornet_concept_map
    Added cz_omop_pcornet_concept_map for chop_start2001_pcornet and
    dcc_start2001_pcornet

---
+++ v2 to v3/ETL Scripts/start2001/ETL Scripts/chop/cz_omop_pcornet_concept_map_ddl.sql
@@ -0,0 +1,10 @@
+CREATE TABLE chop_start2001_pcornet.cz_omop_pcornet_concept_map
+(
+  target_concept character varying(200),
+  source_concept_class character varying(200),
+  source_concept_id character varying(200),
+  value_as_concept_id bigint, 
+  concept_description character varying(200)
+)
+
+
