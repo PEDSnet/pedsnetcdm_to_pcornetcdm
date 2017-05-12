a4694d5e1c9476cf09e324e5eb772aabdd4f8a51
writetoritu <kharer@email.chop.edu>
2017-05-12 12:03:45 -0400

    addressing issue-164

--- v2 to v3.1/ETL Scripts/start2001/Prescribing_ETL.sql
+++ v2 to v3.1/ETL Scripts/start2001/Prescribing_ETL.sql
@@ -1,4 +1,10 @@
--- PRESCRIBING table
+﻿-- PRESCRIBING table
+ALTER TABLE dcc_3dot1_start2001_pcornet.prescribing ALTER raw_rxnorm_cui SET DATA TYPE character varying(20); 
+
+ALTER TABLE dcc_3dot1_start2001_pcornet.prescribing ALTER rx_quantity SET DATA TYPE NUMERIC(20,2);
+ALTER TABLE dcc_3dot1_start2001_pcornet.prescribing ALTER rx_refills SET DATA TYPE NUMERIC(20,2);
+ALTER TABLE dcc_3dot1_start2001_pcornet.prescribing ALTER rx_days_supply SET DATA TYPE NUMERIC(20,2);
+
 
 insert into dcc_3dot1_start2001_pcornet.prescribing (prescribingid,
             patid, encounterid, 
@@ -7,7 +13,12 @@ insert into dcc_3dot1_start2001_pcornet.prescribing (prescribingid,
             rxnorm_cui,
             raw_rx_med_name, raw_rx_frequency, raw_rxnorm_cui,site)
 select 
-	* from dcc_3dot1_pcornet.prescribing
+	prescribingid,
+            patid, encounterid, 
+            rx_providerid, rx_order_date, rx_order_time,
+            rx_start_date, rx_end_date, rx_quantity, rx_refills, rx_days_supply, rx_frequency, rx_basis,
+            rxnorm_cui,
+            raw_rx_med_name, raw_rx_frequency, raw_rxnorm_cui,site from dcc_3dot1_pcornet.prescribing
 where
 	patid IN (select cast(person_id as text) from dcc_3dot1_start2001_pcornet.person_visit_start2001) and EXTRACT(YEAR FROM rx_order_date) >= 2001;
 
