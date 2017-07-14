58dcfc909caed6589e386dcd4a34ea53396246b4
writetoritu <kharer@email.chop.edu>
2017-07-14 10:50:22 -0400

    considering condition_source_value new format

--- v2.5_to_v3.1/ETL Scripts/Condition_ETL.sql
+++ v2.5_to_v3.1/ETL Scripts/Condition_ETL.sql
@@ -18,7 +18,10 @@ select distinct
 		c2.concept_code
 		else case when co.condition_concept_id>0
 		 then c1.concept_code 
-		 else case when condition_source_Value  like '%|%' then trim(split_part(condition_source_value,'|',2))
+		 else case when condition_source_Value  like '%|%' then 
+		 					case when co.site = 'stlouis' then trim(split_part(condition_source_value,'|',3))
+		 					else 
+		 					trim(split_part(condition_source_value,'|',2)) end 
 				else  trim(condition_source_value)  end  end end 
 	 as condition,
 	case when c2.vocabulary_id = 'ICD9CM'  then '09' 
