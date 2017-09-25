f9ad583e3dadcfcd65e6acc0b5ace3eb9eda67fe
ChavanShweta <shweta_pcj@yahoo.com>
2017-09-25 15:43:51 -0400

    Update Prescribing_ETL.sql

--- v2.6_to_3.1/ETL Scripts/Prescribing_ETL.sql
+++ v2.6_to_3.1/ETL Scripts/Prescribing_ETL.sql
@@ -37,7 +37,7 @@ from
 	dcc_pedsnet.drug_exposure de
 	join dcc_3dot1_pcornet.demographic d on d.patid = cast(de.person_id as text)
 	join dcc_3dot1_pcornet.encounter e on cast(de.visit_occurrence_id as text) = e.encounterid
-	left join dcc_3dot1_pcornet.pedsnet_pcornet_valuset_map m1 on case when de.drug_type_concept_id is null 
+	left join dcc_3dot1_pcornet.pedsnet_pcornet_valueset_map m1 on case when de.drug_type_concept_id is null 
 		AND m1.source_concept_id is null then true else cast(de.drug_type_concept_id as text) = m1.source_concept_id end and m1.source_concept_class='prescribing'
 	left join vocabulary.concept c1 on de.drug_concept_id = c1.concept_id AND vocabulary_id = 'RxNorm'
 	left join vocabulary.concept c2 on de.drug_source_concept_id = c2.concept_id
