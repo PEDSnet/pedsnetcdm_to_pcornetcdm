8740e026cdc477004a92ac9418f98082aba3af4e
Chavan, Shweta <CHAVANS@EMAIL.CHOP.EDU>
2017-09-27 16:00:04 -0400

    Code updated
    Following Changes were made:
    - added permissions to pcornet sas user
    - added code to populate the harvest data
    - Added changes to the permission for pcor etl user.

--- v2.6_to_3.1/pcornet_loading/loading/main.py
+++ v2.6_to_3.1/pcornet_loading/loading/main.py
@@ -1,10 +1,13 @@
+# region import
 import ConfigParser
+import re
 import click
 import psycopg2
 import time
 import os
 import config
 import query
+# endregion
 
 configfile_name = "database.ini"
 
@@ -19,7 +22,7 @@ configfile_name = "database.ini"
 @click.option('--host', '-h', default=False, help='The Server name ex. dev01')
 def cli(searchpath, pwprompt, user, database, host):
     """This tool is used to load the data"""
-
+    # region verify
     if not user:
         click.echo('Please provide the database username.')
         user = click.prompt('Username', hide_input=False)
@@ -36,13 +39,13 @@ def cli(searchpath, pwprompt, user, database, host):
 
     if not searchpath:
         searchpath = click.prompt('schema name', hide_input=False)
+    # endregion
 
     # Check if there is already a configuration file
     if os.path.isfile(configfile_name):
-        # delete the file
         os.remove(configfile_name)
+
     if not os.path.isfile(configfile_name):
-        # Create the configuration file as it doesnt exist yet
         cfgfile = open(configfile_name, 'w')
 
         configini = ConfigParser.ConfigParser()
@@ -64,61 +67,158 @@ def cli(searchpath, pwprompt, user, database, host):
 def connect():
     '''This function is used to connect to the database and query the database'''
     conn = None
+
     try:
-        # read connection parameters
+        # region read connection parameters
         params = config.config('db')
         schema_path = config.config('schema')
         # schema = schema_path['schema']+"""_3dot1_pcornet"""
-        schema = [schema_path['schema'] + """_3dot1_pcornet""", schema_path['schema'] + """_start2001_pcornet"""]
+        schema = [(re.sub('_pedsnet', '', schema_path['schema']) + """_3dot1_pcornet"""),
+                  (re.sub('_pedsnet', '', schema_path['schema']) + """_3dot1_start2001_pcornet""")]
+        # endregion
 
-        # connect to the PostgreSQL server
+        # region connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
         # create a cursor
         cur = conn.cursor()
+        # endregion
 
-        # Check if there is already a configuration file
+        # region Check if there is already a configuration file
         if os.path.isfile(configfile_name):
             # delete the file
             os.remove(configfile_name)
+        # endregion
 
         for schemas in schema:
-            # check if the schema exisit
+            # region check if the schema exisit
             cur.execute(
                 """select exists(select 1 from information_schema.schemata where schema_name = \'""" + schemas + """\');""")
             schema_exist = cur.fetchall()[0]
-            print(schema_exist)
 
             if "True" not in schema_exist:
+                print '% schema does not exist..... \n Creating schema ....' % schemas
                 cur.execute(query.create_schema(schemas))
-                print '% schema created' % schema
-
+                print '% schema created' % schemas
+                conn.commit()
             # set the search pat to the schema
             cur.execute("""SET search_path TO """ + schemas + """;""")
-
-            # cur.execute("""SHOW search_path;""")
-            # path = cur.fetchall()
-            # print(path)
             time.sleep(0.1)
+            # endregion
 
-            # create table
+            # region create mapping table
             try:
+                print '\ncreating and populating the mapping table ...'
                 cur.execute(query.create_table(schemas))
-                print(conn.notices)
                 conn.commit()
 
-                # import the file to the database
-                if os.path.isfile('concept_map/concept_map.csv'):
-                    f = open('concept_map/concept_map.csv', 'r')
-                    cur.copy_from(f, schemas + ".pedsnet_pcornet_valueset_map", columns=(
-                        "target_concept", "source_concept_class", "source_concept_id", "value_as_concept_id",
-                        "concept_description"),
+                # region import the file to the database
+                if os.path.isfile('data/concept_map.csv'):
+                    f = open('data/concept_map.csv', 'r')
+                    cur.copy_from(f, schemas + ".pedsnet_pcornet_valueset_map", columns=("target_concept",
+                                                                                         "source_concept_class",
+                                                                                         "source_concept_id",
+                                                                                         "value_as_concept_id",
+                                                                                         "concept_description"),
                                   sep=",")
-                    print(conn.notices)
                     conn.commit()
             except (Exception, psycopg2.OperationalError) as error:
                 print(error)
-        # close the communication with the PostgreSQL
+            # endregion
+
+            # region run the DDL
+            try:
+                print '\nRunning the DDL ...'
+                # set the search pat to the schema
+                cur.execute("""SET search_path TO """ + schemas + """;""")
+                time.sleep(0.1)
+                cur.execute(query.dll())
+                conn.commit()
+            except (Exception, psycopg2.OperationalError) as error:
+                print(error)
+            # endregion
+
+            # region Alter tables and add site column
+            try:
+                print '\nAltering table, adding {site} column ...'
+                cur.execute("""SET search_path TO """ + schemas + """;""")
+                cur.execute(query.site_col(schemas))
+                conn.commit()
+            except(Exception, psycopg2.OperationalError) as error:
+                print(error)
+            # endregion
+
+            # region permissions
+            try:
+                print '\nSetting permissions'
+                cur.execute("""SET search_path TO """ + schemas + """;""")
+                time.sleep(0.1)
+                cur.execute(query.permission(schemas))
+                conn.commit()
+            except(Exception, psycopg2.OperationalError) as error:
+                print(error)
+            # endregion
+
+            # region Alter owner of tables
+            try:
+                cur.execute("""SET search_path TO """ + schemas + """;""")
+                time.sleep(0.1)
+                cur.execute(query.owner(schemas))
+                conn.commit()
+            except(Exception, psycopg2.OperationalError) as error:
+                print(error)
+            # endregion
+
+            # region Populate Harvest
+            try:
+                print '\nPopulating harvest table... '
+                if os.path.isfile('data/harvest_data.csv'):
+                    f = open('data/harvest_data.csv', 'r')
+                    cur.copy_from(f, schemas + ".harvest", columns=("admit_date_mgmt",
+                                                                    "birth_date_mgmt",
+                                                                    "cdm_version",
+                                                                    "datamart_claims",
+                                                                    "datamart_ehr",
+                                                                    "datamart_name",
+                                                                    "datamart_platform",
+                                                                    "datamartid",
+                                                                    "discharge_date_mgmt",
+                                                                    "dispense_date_mgmt",
+                                                                    "enr_end_date_mgmt",
+                                                                    "enr_start_date_mgmt",
+                                                                    "lab_order_date_mgmt",
+                                                                    "measure_date_mgmt",
+                                                                    "network_name", "networkid",
+                                                                    "onset_date_mgmt",
+                                                                    "pro_date_mgmt",
+                                                                    "px_date_mgmt",
+                                                                    "refresh_condition_date",
+                                                                    "refresh_death_cause_date",
+                                                                    "refresh_death_date",
+                                                                    "refresh_demographic_date",
+                                                                    "refresh_diagnosis_date",
+                                                                    "refresh_dispensing_date",
+                                                                    "refresh_encounter_date",
+                                                                    "refresh_enrollment_date",
+                                                                    "refresh_lab_result_cm_date",
+                                                                    "refresh_pcornet_trial_date",
+                                                                    "refresh_prescribing_date",
+                                                                    "refresh_pro_cm_date",
+                                                                    "refresh_procedures_date",
+                                                                    "refresh_vital_date",
+                                                                    "report_date_mgmt",
+                                                                    "resolve_date_mgmt",
+                                                                    "result_date_mgmt",
+                                                                    "rx_end_date_mgmt",
+                                                                    "rx_order_date_mgmt",
+                                                                    "rx_start_date_mgmt",
+                                                                    "specimen_date_mgmt"),
+                                  sep=",")
+                    conn.commit()
+            except (Exception, psycopg2.OperationalError) as error:
+                print(error)
+            # endregion
+        print '\nPcornet data model set up complete ... \nClosing database connection...'
         cur.close()
     except (Exception, psycopg2.OperationalError) as error:
         print(error)
--- v2.6_to_3.1/pcornet_loading/loading/query.py
+++ v2.6_to_3.1/pcornet_loading/loading/query.py
@@ -1,52 +1,95 @@
+# region Imports
 from config import config
 import csv
 import requests
 import os
+import re
+import fileinput
 
-csv_concept = "concept_map.csv"
+# endregion
+
+# region filenames
+csv_concept = "data/concept_map.csv"
 create_table_script = "scripts/create_table.sql"
+site_col_file = "scripts/site_col.sql"
+privileges = "scripts/privileges.sql"
+alt_owner_file = "scripts/alter_tbl_owner.sql"
 
 
-def get_file(uri):
-    if not uri:
-        uri = 'https://raw.githubusercontent.com/PEDSnet/pedsnetcdm_to_pcornetcdm/master/v2.6_to_3.1/pedsnet_pcornet_mappings.txt'
-    r = requests.get(uri)
-    data = csv.reader(r.text.splitlines(), delimiter='|')
-    if not os.path.isfile(csv_concept):
-        csv_file = csv.writer(open('concept_map/concept_map.csv', 'wb'))
-        csv_file.writerows(data)
+# endregion
 
-
+# region create valueset map
 def create_table(schema):
-    """ create tables in the PostgreSQL database"""
-
-    commands = """CREATE TABLE IF NOT EXISTS """ + schema + """.pedsnet_pcornet_valueset_map (
-                target_concept character varying(200),
-                source_concept_class character varying(200),
-                source_concept_id character varying(200),
-                value_as_concept_id character varying(200),
-                concept_description character varying(200)
-                );
-
-                ALTER TABLE """ + schema + """.pedsnet_pcornet_valueset_map
-                OWNER to pcor_et_user;
-
-                GRANT SELECT ON TABLE """ + schema + """.pedsnet_pcornet_valueset_map TO pcornet_sas;
-
-                GRANT SELECT ON TABLE """ + schema + """.pedsnet_pcornet_valueset_map TO peds_staff;
-
-                GRANT ALL ON TABLE """ + schema + """.pedsnet_pcornet_valueset_map TO pcor_et_user;
-
-                GRANT ALL ON TABLE """ + schema + """.pedsnet_pcornet_valueset_map TO dcc_owner; """
-    with open(create_table_script, 'wb') as f:
-        f.writelines(commands)
+    """
+    create tables in the PostgreSQL database in specified schema
+    """
+    for line in fileinput.input(create_table_script, inplace=1, backup='.bak'):
+        line = re.sub('EXISTS .*?\.', "EXISTS " + schema + ".", line.rstrip())
+        print(line)
+    with open(create_table_script, 'r') as valueset_file:
+        commands = valueset_file.read()
     return commands
 
 
+# endregion
+
+# region Schema Creation
 def create_schema(schema):
     """creates schema if not exists"""
-
     command = """CREATE SCHEMA IF NOT EXISTS """ + schema + """ AUTHORIZATION pcor_et_user;
-                 GRANT USAGE ON SCHEMA """ + schema + """ TO dcc_owner;
-                 GRANT ALL ON schema """ + schema + """ TO dcc_owner;"""
+                 GRANT USAGE ON SCHEMA """ + schema + """ TO pcornet_sas;
+                 GRANT ALL ON SCHEMA """ + schema + """ TO dcc_owner;
+                 GRANT ALL ON SCHEMA """ + schema + """ TO pcor_et_user;
+                 """
     return command
+
+
+# endregion
+
+# region create DDL
+def dll():
+    """Creates dll for pcornet"""
+    try:
+        dll_url = 'http://data-models-sqlalchemy.research.chop.edu/pcornet/3.1.0/ddl/postgresql/tables'
+        dll_script = requests.get(dll_url).text
+        return dll_script
+    except (Exception, requests.ConnectionError) as e:
+        print(e)
+
+
+# endregion
+
+# region Alter site column
+def site_col(schema):
+    """This function alters the table and creates the site columns"""
+    # Replace variables in file
+    for line in fileinput.input(site_col_file, inplace=1, backup='.bak'):
+        line = re.sub('table .*?\.', "table " + schema + ".", line.rstrip())
+        print(line)
+    with open(site_col_file, 'r') as site_file:
+        alter_site_col = site_file.read()
+    return alter_site_col
+
+
+# endregion
+
+# region Set the privileges
+def permission(schema):
+    """This function sets up the permissions to the schemas"""
+    for line in fileinput.input(privileges, inplace=1, backup='.bak'):
+        line = re.sub('SCHEMA .*?\ TO', "SCHEMA " + schema + " TO", line.rstrip())
+        print(line)
+    with open(privileges, 'r') as perm_file:
+        privilege = perm_file.read()
+    return privilege
+
+
+# endregion
+
+# region Alter table owner
+def owner(schema):
+    """This function returns the sql script to change the owner of all schemas"""
+    with open(alt_owner_file, 'r') as owner_file:
+        alter_owner = owner_file.read()
+    return alter_owner + "select alter_tbl_owner('" + schema + "')"
+# endregion
