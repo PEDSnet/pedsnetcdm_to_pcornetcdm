6c3e50837fd575db88123140b7ec184c4486b988
Chavan, Shweta <CHAVANS@EMAIL.CHOP.EDU>
2017-10-24 16:00:09 -0400

    PCORnet v2.7_to_3.1
    fixes #331
    
    Following options has been implemented:
    - DDL
    - update value set map
    - truncate all tables and remove FK’s

---
+++ v2.7_to_3.1/pcornet_loading/scripts/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS dcc_3dot1_start2001_pcornet.pedsnet_pcornet_valueset_map (
+                target_concept character varying(200),
+                source_concept_class character varying(200),
+                source_concept_id character varying(200),
+                value_as_concept_id character varying(200),
+                concept_description character varying(200)
+                );
