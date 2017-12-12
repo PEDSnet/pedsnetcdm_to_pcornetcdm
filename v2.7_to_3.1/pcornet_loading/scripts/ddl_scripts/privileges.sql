09abc620be5267d477cd168ba2e3b377df0198c9
Chavan, Shweta <CHAVANS@EMAIL.CHOP.EDU>
2017-12-12 12:41:36 -0500

    updated code
    fixed the errors for full etc run

--- v2.7_to_3.1/pcornet_loading/scripts/ddl_scripts/privileges.sql
+++ v2.7_to_3.1/pcornet_loading/scripts/ddl_scripts/privileges.sql
@@ -1,5 +1,5 @@
 -- Set up the privilege to the user
-GRANT ALL ON SCHEMA dcc_3dot1_start2001_pcornet TO pcor_et_user;
-GRANT USAGE ON  SCHEMA dcc_3dot1_start2001_pcornet TO pcornet_sas;
-GRANT ALL ON ALL TABLES IN SCHEMA dcc_3dot1_start2001_pcornet TO pcor_et_user;
-GRANT SELECT ON ALL TABLES IN SCHEMA dcc_3dot1_start2001_pcornet TO pcornet_sas;
+GRANT ALL ON SCHEMA nationwide_3dot1_start2001_pcornet TO pcor_et_user;
+GRANT USAGE ON  SCHEMA nationwide_3dot1_start2001_pcornet TO pcornet_sas;
+GRANT ALL ON ALL TABLES IN SCHEMA nationwide_3dot1_start2001_pcornet TO pcor_et_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA nationwide_3dot1_start2001_pcornet TO pcornet_sas;
