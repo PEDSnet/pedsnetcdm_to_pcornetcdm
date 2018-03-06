3da737466815d25d259a9cad466327d27b451197
ChavanShweta <shweta_pcj@yahoo.com>
2018-03-06 14:21:05 -0500

    Update privileges.sql
    removed the site names from the script

--- v2.8_to_3.1/sql_etl/scripts/ddl_scripts/privileges.sql
+++ v2.8_to_3.1/sql_etl/scripts/ddl_scripts/privileges.sql
@@ -1,5 +1,5 @@
 -- Set up the privilege to the user
-GRANT ALL ON SCHEMA stlouis_pcornet TO pcor_et_user;
-GRANT USAGE ON  SCHEMA stlouis_pcornet TO pcornet_sas;
-GRANT ALL ON ALL TABLES IN SCHEMA stlouis_pcornet TO pcor_et_user;
-GRANT SELECT ON ALL TABLES IN SCHEMA stlouis_pcornet TO pcornet_sas;
+GRANT ALL ON SCHEMA SITE_pcornet TO pcor_et_user;
+GRANT USAGE ON  SCHEMA SITE_pcornet TO pcornet_sas;
+GRANT ALL ON ALL TABLES IN SCHEMA SITE_pcornet TO pcor_et_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA SITE_pcornet TO pcornet_sas;
