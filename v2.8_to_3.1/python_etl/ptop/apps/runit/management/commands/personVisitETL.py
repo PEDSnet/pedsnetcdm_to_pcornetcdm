f0e4aaabaaeb5f7aec94bc14bee96456560e7942
piglet25 <shague@stevens.edu>
2018-04-04 17:29:27 -0400

    updated person visit, added chunk size

--- v2.8_to_3.1/python_etl/ptop/apps/runit/management/commands/personVisitETL.py
+++ v2.8_to_3.1/python_etl/ptop/apps/runit/management/commands/personVisitETL.py
@@ -50,14 +50,14 @@ class Command(BaseCommand):
         pedsnet_session = init_pedsnet(connection)
         init_pcornet(connection)
 
-        df = pd.read_sql(pedsnet_session.query(VisitOccurrence.person_id,
-                                               VisitOccurrence.visit_occurrence_id.label('visit_id')) \
-                         .filter(extract('year', VisitOccurrence.visit_start_date) >= 2001).statement,
-                         pedsnet_session.bind)
+        for df in pd.read_sql(pedsnet_session.query(VisitOccurrence.person_id,
+                                                    VisitOccurrence.visit_occurrence_id.label('visit_id')) \
+                                      .filter(extract('year', VisitOccurrence.visit_start_date) >= 2001).statement,
+                              pedsnet_session.bind, chunksize=5000):
 
-        odo(df, PersonVisit.__table__,
-            dshape='var * {person_id: int, visit_id: int}'
-            )
+            odo(df, PersonVisit.__table__,
+                dshape='var * {person_id: int, visit_id: int}'
+                )
 
         # close session
         pedsnet_session.close()
