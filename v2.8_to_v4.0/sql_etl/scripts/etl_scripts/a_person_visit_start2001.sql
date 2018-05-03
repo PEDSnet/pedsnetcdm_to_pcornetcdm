389d077eede6a9f838986994edef34c5d89cb64b
Khare <kharer@email.chop.edu>
2018-05-03 15:31:11 -0400

    adding scripts for new cdm4.0

---
+++ v2.8_to_v4.0/sql_etl/scripts/etl_scripts/a_person_visit_start2001.sql
@@ -0,0 +1,13 @@
+begin;
+
+CREATE TABLE SITE_4dot0_pcornet.person_visit_start2001
+AS
+SELECT person_id, visit_occurrence_id AS visit_id
+FROM SITE_pedsnet.visit_occurrence
+WHERE EXTRACT(YEAR FROM visit_start_date) >= 2001;
+
+ALTER TABLE SITE_pcornet.person_visit_start2001
+ADD CONSTRAINT xpk_person_visit_start2001
+PRIMARY KEY (visit_id);
+
+commit;
