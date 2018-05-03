52b8faa12642d10a50be54d70a2ffb4dadcf6797
Chavan, Shweta <CHAVANS@EMAIL.CHOP.EDU>
2018-05-03 13:12:30 -0400

    script to combine the concept map

---
+++ v2.8_to_v4.0/sql_etl/scripts/ddl_scripts/privileges.sql
@@ -0,0 +1,5 @@
+-- Set up the privilege to the user
+GRANT ALL ON SCHEMA stlouis_4dot0_pcornet TO pcor_et_user;
+GRANT USAGE ON  SCHEMA stlouis_4dot0_pcornet TO pcornet_sas;
+GRANT ALL ON ALL TABLES IN SCHEMA stlouis_4dot0_pcornet TO pcor_et_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA stlouis_4dot0_pcornet TO pcornet_sas;
