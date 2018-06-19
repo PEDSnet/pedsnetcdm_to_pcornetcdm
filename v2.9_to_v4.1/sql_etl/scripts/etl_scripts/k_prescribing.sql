3401f1da1c35660bd9ef3d00f96263e9881075ac
Chavan, Shweta <CHAVANS@EMAIL.CHOP.EDU>
2018-06-19 09:44:43 -0400

    updating the changes
    changing the raw_rx_med_name field

--- v2.9_to_v4.1/sql_etl/scripts/etl_scripts/k_prescribing.sql
+++ v2.9_to_v4.1/sql_etl/scripts/etl_scripts/k_prescribing.sql
@@ -60,7 +60,9 @@ select
 	CAST(nullif(c1.concept_code, '') AS integer) as rxnorm_cui,
 	'OD' as rx_source, 
 	coalesce(m5.target_concept,'OT') as rx_dispense_as_written, -- extracting from pedsnet dispense_as_written_concept_id column data in pcornet valueset
-	c1.concept_name as raw_rx_med_name,
+	case when (c1.concept_name is null) then split_part(drug_source_value,'|',1)) --- extract from drug source value
+		 else c1.concept_name
+		 end as raw_rx_med_name,
 	de.frequency as raw_rx_frequency,
 	c2.concept_code as raw_rxnorm_cui,
 	eff_drug_dose_source_value as raw_rx_dose_ordered,
@@ -71,7 +73,7 @@ select
 from
 	SITE_pedsnet.drug_exposure de
 	left join vocabulary.concept c1 on de.drug_concept_id = c1.concept_id AND
-	                                   domain_id = 'Drug'
+	                                   `vocabulary_id = 'RxNorm'
 	left join vocabulary.concept c2 on de.drug_source_concept_id = c2.concept_id
 	left join SITE_pcornet.rx_dose_form_data rdf on de.drug_concept_id =  rdf.drug_concept_id
 	left join pcornet_maps.pedsnet_pcornet_valueset_map m1 on cast(dose_unit_concept_id as text) = m1.source_concept_id 
@@ -82,7 +84,7 @@ from
 			                                               and m3.source_concept_class='Rx Frequency'
 	left join pcornet_maps.pedsnet_pcornet_valueset_map m4 on cast(de.route_concept_id as text) = m4.source_concept_id 
 			                                               and m4.source_concept_class='Route'
-	left join pcornet_maps.pedsnet_pcornet_valueset_map m5 on  cast(co.dispense_as_written_concept_id as text) = m5.source_concept_id  and
+	left join pcornet_maps.pedsnet_pcornet_valueset_map m5 on  cast(de.dispense_as_written_concept_id as text) = m5.source_concept_id  and
 	                                                                 m5.source_concept_class='dispense written'
 where
 	de.drug_type_concept_id IN ('38000177')
