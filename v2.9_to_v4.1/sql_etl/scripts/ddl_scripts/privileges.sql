239b9ef5006d385e7e85b5bd5851162bcfc81782
Chavan, Shweta <CHAVANS@EMAIL.CHOP.EDU>
2018-07-03 14:10:53 -0400

    removing duplicate entries for urine
    PCO-296
    fixes #423
    removing the duplicate values for urine specimen.

--- v2.9_to_v4.1/sql_etl/scripts/ddl_scripts/privileges.sql
+++ v2.9_to_v4.1/sql_etl/scripts/ddl_scripts/privileges.sql
@@ -1,5 +1,5 @@
 -- Set up the privilege to the user
-GRANT ALL ON SCHEMA SITE TO pcor_et_user;
-GRANT USAGE ON  SCHEMA SITE TO pcornet_sas;
-GRANT ALL ON ALL TABLES IN SCHEMA SITE TO pcor_et_user;
-GRANT SELECT ON ALL TABLES IN SCHEMA SITE TO pcornet_sas;
+GRANT ALL ON SCHEMA nemours_pcornet TO pcor_et_user;
+GRANT USAGE ON  SCHEMA nemours_pcornet TO pcornet_sas;
+GRANT ALL ON ALL TABLES IN SCHEMA nemours_pcornet TO pcor_et_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA nemours_pcornet TO pcornet_sas;
