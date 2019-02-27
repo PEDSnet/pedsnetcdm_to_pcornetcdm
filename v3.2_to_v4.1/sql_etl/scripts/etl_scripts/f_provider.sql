695bbf62cc62c88f690ae8907858bc3486b8be18
Chavan, Shweta <CHAVANS@EMAIL.CHOP.EDU>
2019-02-27 16:19:42 -0500

    updating the provider script
    fixes #459
    OP-240

--- v3.2_to_v4.1/sql_etl/scripts/etl_scripts/f_provider.sql
+++ v3.2_to_v4.1/sql_etl/scripts/etl_scripts/f_provider.sql
@@ -8,11 +8,10 @@ Insert into SITE_pcornet.provider
 	provider_npi_flag, 
 	raw_provider_specialty_primary, site
 )
-Select
-	p.provider_id as providerid,
+Select distinct on (p.provider_id) p.provider_id as providerid,
 	coalesce(m1.target_concept,'OT') as provider_sex,
-	coalesce(m2.target_concept,'OT') as provider_specialty_primary, 
-	null as provider_npi,
+	coalesce(m.target_concept,'OT') as provider_specialty_primary, 
+	null::numeric as provider_npi,
 	'N' as provider_npi_flag,
 	p.specialty_source_value as raw_provider_specialty_primary,
 	p.site as site 
@@ -20,7 +19,9 @@ From
 	SITE_pedsnet.provider p
 	left join pcornet_maps.pedsnet_pcornet_valueset_map m1 on m1.source_concept_class='Gender' and
 	cast(p.gender_concept_id as text) = m1.source_concept_id
-	left join pcornet_maps.pedsnet_pcornet_valueset_map m2 on m1.source_concept_class='Provider Specialty' and
-	cast(p.specialty_concept_id as text) = m1.source_concept_id ;
+	left join pcornet_maps.pedsnet_pcornet_valueset_map m on m.source_concept_class='Provider Specialty' 
+        and case when m.source_concept_id != '' then m.source_concept_id = p.specialty_concept_id::text 
+                                                else m.concept_description LIKE '%' || specialty_concept_name || '%'
+            end;
 	
 commit;
