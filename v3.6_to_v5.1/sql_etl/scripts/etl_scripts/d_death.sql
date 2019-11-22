a86872ed7e792410c4a95cb42bc59a5ce48f0b45
Chavan, Shweta <CHAVANS@EMAIL.CHOP.EDU>
2019-11-22 14:20:02 -0500

    adding the folder
    OP-597

---
+++ v3.6_to_v5.1/sql_etl/scripts/etl_scripts/d_death.sql
@@ -0,0 +1,24 @@
+begin;
+
+Insert into SITE_pcornet.death(
+	patid, death_date, death_date_impute,
+	death_source, death_match_confidence, site
+)
+Select
+	de.person_id as patid,
+	de.death_date as death_date,
+	coalesce(m1.target_concept,'OT') as death_impute,
+	'L' as death_source, --  default for now until new conventions
+	null as death_match_confidence, --  we do not capture it dicretely in the EHRs
+	'SITE' as site -- retrieve one record in case of multiple death causes
+From
+	SITE_pedsnet.death de
+	left join pcornet_maps.pedsnet_pcornet_valueset_map m1 on m1.source_concept_class='Death date impute' and
+	                                                          cast(de.death_impute_concept_id as text) = m1.source_concept_id
+Where
+	de.death_type_concept_id  = 38003569 and
+	person_id IN (select person_id from SITE_pcornet.person_visit_start2001)
+group by de.person_id , de.death_date, coalesce(m1.target_concept,'OT');
+
+
+commit;
