662ae3ef17ae97a7ccbe1022b7c58a6f9ef450ba
Chavan, Shweta <CHAVANS@EMAIL.CHOP.EDU>
2020-05-17 20:00:47 -0400

    updating obs_gen
    OP-829
    updating the obs_gen for the suggestion on PR

--- v3.8_to_v5.1/sql_etl/scripts/etl_scripts/n_obs_gen.sql
+++ v3.8_to_v5.1/sql_etl/scripts/etl_scripts/n_obs_gen.sql
@@ -1,50 +1,99 @@
 begin;
+create sequence obs_gen_seq_id start 1;
+commit;
 
-INSERT INTO SITE_pcornet.obs_gen(
-	obsgenid, patid, encounterid, obsgen_providerid,  obsgen_date,obsgen_time, obsgen_type, obsgen_code, obsgen_result_qual, 
-	 obsgen_result_text, obsgen_result_num,obsgen_result_modifier,  obsgen_result_unit, obsgen_table_modified, 
-	obsgen_id_modified, obsgen_source,raw_obsgen_name,raw_obsgen_type, raw_obsgen_code,  raw_obsgen_result, 
-	raw_obsgen_unit,  site)
+begin;
+Create table SITE_pcornet.filter_obs
+as
+select * 
+from SITE_pedsnet.observation obs
+where EXTRACT(YEAR FROM obs.observation_date)>=2001
+and obs.person_id in (select person_id from SITE_pcornet.person_visit_start2001)
+and (obs.visit_occurrence_id in (select visit_id from SITE_pcornet.person_visit_start2001)
+	 or obs.visit_occurrence_id is null);
+commit;
+
+begin;
+create table SITE_pcornet.adt_obs
+as
 select distinct on (obs.observation_id)obs.observation_id::text as obsgenid,
 obs.person_id::text as patid,
 obs.visit_occurrence_id::text as encounterid,
 obs.provider_id as obsgen_providerid,
 obs.observation_date::date as obsgen_date,
 LPAD(date_part('hour',observation_datetime)::text,2,'0')||':'||LPAD(date_part('minute',observation_datetime)::text,2,'0') as obsgen_time,
-case when adt.service_concept_id in (2000000079,2000000080,2000000078) then 'PC_COVID' 
-      when meas.measurement_concept_id in (2000001422) then 'PC_COVID'
-      else null end as obsgen_type,
-case when adt.service_concept_id in (2000000079,2000000080,2000000078) then '2000' 
-      when meas.measurement_concept_id in (2000001422) then '1000'
-      else null end as obsgen_code,
+'PC_COVID' as obsgen_type,
+'2000' as obsgen_code,
 null as obsgen_result_qual,
-case when meas.measurement_concept_id in (2000001422) then obs.value_as_string
-else null end  as obsgen_result_text,
-case when meas.measurement_concept_id in (2000001422) then obs.value_as_number
-else null end as obsgen_result_num,
+null as obsgen_result_text,
+null as obsgen_result_num,
 null as obsgen_result_modifier,
-case when meas.measurement_concept_id in (2000001422) then map.target_concept
-else null end as obsgen_result_unit,
+null as obsgen_result_unit,
 null as obsgen_table_modified,
 null as obsgen_id_modified,
-case when adt.service_concept_id in (2000000079,2000000080,2000000078) then 'DR'
-      when meas.measurement_concept_id in (2000001422) then 'OD'
-      else null end  as obsgen_source,
+'DR' as obsgen_source,
 null as raw_obsgen_name,
 null as raw_obsgen_type,
 null as raw_obsgen_code,
-case when meas.measurement_concept_id in (2000001422) then obs.value_as_string
-else null end  as raw_obsgen_result,
-case when meas.measurement_concept_id in (2000001422) then obs.unit_source_value
-else null end as raw_obsgen_unit,
+null as raw_obsgen_result,
+null as raw_obsgen_unit,
 obs.site
-from SITE_pedsnet.observation obs
-left join SITE_pcornet.encounter enc on enc.encounterid::int = obs.visit_occurrence_id 
-left join SITE_pedsnet.adt_occurrence adt on adt.visit_occurrence_id = enc.encounterid::int and adt.service_concept_id in (2000000079,2000000080,2000000078)
-left join SITE_pedsnet.measurement meas on meas.person_id = obs.person_id and meas.visit_occurrence_id = obs.visit_occurrence_id and meas.measurement_concept_id in (2000001422)
-left join pcornet_maps.pedsnet_pcornet_valueset_map map on map.source_concept_id = obs.unit_concept_id::text and map.source_concept_class = 'Result unit'
-where EXTRACT(YEAR FROM obs.observation_date)>=2001
-	and obs.person_id in (select person_id from SITE_pcornet.person_visit_start2001);
+from SITE_pedsnet.adt_occurrence adt  
+inner join SITE_pcornet.filter_obs obs on adt.person_id = obs.person_id::int and adt.adt_date = obs.observation_date
+where adt.service_concept_id in (2000000079,2000000080,2000000078);
 
+commit;
+
+begin;
+create table SITE_pcornet.meas_obs
+as
+select distinct on (obs.observation_id)obs.observation_id::text as obsgenid,
+obs.person_id::text as patid,
+obs.visit_occurrence_id::text as encounterid,
+obs.provider_id as obsgen_providerid,
+obs.observation_date::date as obsgen_date,
+LPAD(date_part('hour',observation_datetime)::text,2,'0')||':'||LPAD(date_part('minute',observation_datetime)::text,2,'0') as obsgen_time,
+'PC_COVID' as obsgen_type,
+'1000' as obsgen_code,
+map_qual.target_concept as obsgen_result_qual,
+obs.value_as_string as obsgen_result_text,
+meas.value_as_number as obsgen_result_num,
+map_mod.target_concept as obsgen_result_modifier,
+map.target_concept as obsgen_result_unit,
+null as obsgen_table_modified,
+null as obsgen_id_modified,
+'OD' as obsgen_source,
+null as raw_obsgen_name,
+null as raw_obsgen_type,
+null as raw_obsgen_code,
+meas.value_as_number as raw_obsgen_result,
+meas.unit_concept_name as raw_obsgen_unit,
+obs.site
+from SITE_pedsnet.measurement meas 
+inner join SITE_pcornet.filter_obs obs on meas.person_id = obs.person_id and meas.measurement_date = obs.observation_date 
+left join pcornet_maps.pedsnet_pcornet_valueset_map map on map.source_concept_id = meas.unit_concept_id::text and map.source_concept_class = 'Result unit'
+left join pcornet_maps.pedsnet_pcornet_valueset_map map_mod on map.source_concept_id = meas.operator_concept_id::text and map_mod.source_concept_class = 'Result modifier'
+left join pcornet_maps.pedsnet_pcornet_valueset_map map_qual on cast(meas.value_as_concept_id as text)= map_qual.source_concept_id and map_qual.source_concept_class = 'Result qualifier'
+where meas.measurement_concept_id in (2000001422,4353936);
+
+commit;
+
+begin;
+INSERT INTO SITE_pcornet.obs_gen(
+	obsgenid, patid, encounterid, obsgen_providerid,  obsgen_date,obsgen_time, obsgen_type, obsgen_code, obsgen_result_qual, 
+	 obsgen_result_text, obsgen_result_num,obsgen_result_modifier,  obsgen_result_unit, obsgen_table_modified, 
+	obsgen_id_modified, obsgen_source,raw_obsgen_name,raw_obsgen_type, raw_obsgen_code,  raw_obsgen_result, 
+	raw_obsgen_unit, site)
+select (nextval('obs_gen_seq_id')) as obsgenid, patid, encounterid, obsgen_providerid,  obsgen_date,obsgen_time, obsgen_type, obsgen_code, obsgen_result_qual, 
+	 obsgen_result_text, obsgen_result_num::numeric,obsgen_result_modifier,  obsgen_result_unit, obsgen_table_modified, 
+	obsgen_id_modified, obsgen_source,raw_obsgen_name,raw_obsgen_type, raw_obsgen_code,  raw_obsgen_result, 
+	raw_obsgen_unit,site 
+from SITE_pcornet.adt_obs
+union 
+select (nextval('obs_gen_seq_id')) as obsgenid, patid, encounterid, obsgen_providerid,  obsgen_date,obsgen_time, obsgen_type, obsgen_code, obsgen_result_qual, 
+	 obsgen_result_text, obsgen_result_num,obsgen_result_modifier,  obsgen_result_unit, obsgen_table_modified, 
+	obsgen_id_modified, obsgen_source,raw_obsgen_name,raw_obsgen_type, raw_obsgen_code,  raw_obsgen_result::text, 
+	raw_obsgen_unit,  site 
+from SITE_pcornet.meas_obs;
 
 commit;
