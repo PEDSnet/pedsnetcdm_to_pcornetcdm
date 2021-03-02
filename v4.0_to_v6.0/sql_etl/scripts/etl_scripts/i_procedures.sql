385b06f6ee7dce844ed5e936ce93c5f159c47452
Evanette Burrows <burrowse@email.chop.edu>
2021-03-02 12:48:31 -0500

    Update i_procedures.sql

--- v4.0_to_v6.0/sql_etl/scripts/etl_scripts/i_procedures.sql
+++ v4.0_to_v6.0/sql_etl/scripts/etl_scripts/i_procedures.sql
@@ -4,7 +4,7 @@ insert into SITE_pcornet.procedures(
             proceduresid,patid, encounterid, enc_type, admit_date, providerid, px_date,px, px_type, px_source,
             ppx, raw_ppx,
             raw_px, raw_px_type,site)
-elect distinct
+select distinct
                 cast(procedure_occurrence_id as text) as proceduresid,
                 cast(person_id as text) as patid,
                 cast(visit_occurrence_id as text) as encounterid,
@@ -20,8 +20,8 @@ elect distinct
                 coalesce(px_cd_1.vocabulary_id,px_cd_2.vocabulary_id,'OT') as raw_px_type,
                 procedure_type_concept_id as raw_ppx,
                 'SITE' as site
-from stanford_pedsnet.procedure_occurrence po
-                join stanford_pcornet.encounter enc on cast(po.visit_occurrence_id as text)=enc.encounterid
+from SITE_pedsnet.procedure_occurrence po
+                join SITE_pcornet.encounter enc on cast(po.visit_occurrence_id as text)=enc.encounterid
                 left join vocabulary.concept px_cd_1 on px_cd_1.concept_id = po.procedure_concept_id and px_cd_1.vocabulary_id in ('HCPCS','CPT4','ICD10PCS','SNOMED','ICD9Proc')
     left join vocabulary.concept px_cd_2 on px_cd_2.concept_id = po.procedure_source_concept_id and px_cd_2.vocabulary_id in ('HCPCS','CPT4','ICD10PCS','SNOMED','ICD9Proc')
     left join pcornet_maps.pedsnet_pcornet_valueset_map px_typ on px_typ.source_concept_id = px_cd_1.vocabulary_id
@@ -30,9 +30,9 @@ from stanford_pedsnet.procedure_occurrence po
                                                                                m4.source_concept_class='px source'
                 left join pcornet_maps.pedsnet_pcornet_valueset_map m5 on cast(po.procedure_type_concept_id as text) = m5.source_concept_id AND
                                                                                 m5.source_concept_class='ppx'                                                               
-                where  person_id IN (select person_id from stanford_pcornet.person_visit_start2001)
+                where  person_id IN (select person_id from SITE_pcornet.person_visit_start2001)
                        and EXTRACT(YEAR FROM procedure_date) >= 2001
                        and visit_occurrence_id is not null
-                       and visit_occurrence_id not in (select visit_occurrence_id from stanford_pedsnet.visit_occurrence where
+                       and visit_occurrence_id not in (select visit_occurrence_id from SITE_pedsnet.visit_occurrence where
                                                                                 extract(year from visit_start_date)<2001);  
 commit;
