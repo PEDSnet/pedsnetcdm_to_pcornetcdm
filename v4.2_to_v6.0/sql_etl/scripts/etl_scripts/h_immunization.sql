e3be03bfdb85678e8aeda5a0cc5bb7007b1d2d45
Hague <hagues@reslnpbddb01.research.chop.edu>
2021-08-08 02:41:53 -0400

    added vacuum in immunization not mapped was taking too long

--- v4.2_to_v6.0/sql_etl/scripts/etl_scripts/h_immunization.sql
+++ v4.2_to_v6.0/sql_etl/scripts/etl_scripts/h_immunization.sql
@@ -127,17 +127,3 @@ delete from SITE_pcornet.immunization
 where encounterid IS NOT null and
 	   encounterid::int NOT IN (select visit_id from SITE_pcornet.person_visit_start2001);
 commit;
-
-begin;
-with not_mapping as
-(
-	select immunizationid from SITE_pcornet.immunization where vx_code = ''
-)
-update SITE_pcornet.immunization
-set vx_code = coalesce(c.concept_code,'')
-from not_mapping i
-left join SITE_pedsnet.immunization im on im.immunization_id = i.immunizationid::int
-left join vocabulary.concept c on c.concept_name @@ im.immunization_source_value
-                                and concept_class_id = 'CVX'
-where SITE_pcornet.immunization.immunizationid = i.immunizationid;
-commit;
