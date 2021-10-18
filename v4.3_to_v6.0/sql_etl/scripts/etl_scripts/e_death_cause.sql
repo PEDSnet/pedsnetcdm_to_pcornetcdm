ab17839e991f3cd52a247704df58e3cf9c386faa
Hague <hagues@reslnpbddb01.research.chop.edu>
2021-10-18 10:24:08 -0400

    adjust query using ICD 9/10 codes and date

--- v4.3_to_v6.0/sql_etl/scripts/etl_scripts/e_death_cause.sql
+++ v4.3_to_v6.0/sql_etl/scripts/etl_scripts/e_death_cause.sql
@@ -24,6 +24,7 @@ left(case when c.vocabulary_id in ('ICD9CM', 'ICD10','ICD10CM')
          	       else trim(split_part(cause_source_value,'|',2))
          	  end
  		end
+	end
  	end
     end,8) as death_cause,
 	case when c.vocabulary_id = 'ICD9CM'  then '09'
@@ -43,8 +44,8 @@ left(case when c.vocabulary_id in ('ICD9CM', 'ICD10','ICD10CM')
 	from SITE_pedsnet.death d
 	left join vocabulary.concept c on d.cause_source_concept_id = c.concept_id
 	left join vocabulary.concept c2 on d.cause_concept_id = c2.concept_id
- 	left join vocabulary.concept c3 on d.cause_source_value = c3.concept_code where c3.vocabulary_id='ICD9CM' and d.death_date < '2015-10-01'
- 	left join vocabulary.concept c4 on d.cause_source_value = c4.concept_code where c4.vocabulary_id='ICD10CM' and d.death_date >= '2015-10-01' 
+ 	left join vocabulary.concept c3 on d.cause_source_value = c3.concept_code and c3.vocabulary_id='ICD9CM' and d.death_date < '2015-10-01'
+ 	left join vocabulary.concept c4 on d.cause_source_value = c4.concept_code and c4.vocabulary_id='ICD10CM' and d.death_date >= '2015-10-01' 
 	where cause_source_value is not null 
 	and  cause_source_concept_id<>44814650
 	and cause_source_value not in ('NI', -- nemours
