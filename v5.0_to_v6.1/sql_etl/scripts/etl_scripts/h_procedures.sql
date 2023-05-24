63ac26998bac35d36e9b712d2523bb409993bf28
Sam Boss <bossa@chop.edu>
2023-05-24 13:56:52 -0400

    fix runtime bug

--- v5.0_to_v6.1/sql_etl/scripts/etl_scripts/h_procedures.sql
+++ v5.0_to_v6.1/sql_etl/scripts/etl_scripts/h_procedures.sql
@@ -6,7 +6,7 @@ insert into SITE_pcornet.procedures(
             raw_px, raw_px_type,site)
 select distinct
 	cast(procedure_occurrence_id as text) as proceduresid,
-	cast(person_id as text) as patid,
+	cast(po.person_id as text) as patid,
 	cast(visit_occurrence_id as text) as encounterid,
 	enc.enc_type as enc_type,
 	enc.admit_date as admit_date,
@@ -16,8 +16,7 @@ select distinct
 		px_cd_1.concept_code, 
 		px_cd_2.concept_code, 
 		case 
-			when left(split_part(procedure_source_value,'|',2),11) = ' ' 
-			or left(split_part(procedure_source_value,'|',2),11) = '' then 'NI'
+			when left(split_part(procedure_source_value,'|',2),11) = ' ' or left(split_part(procedure_source_value,'|',2),11) = '' then 'NI'
 			else LTRIM(left(split_part(procedure_source_value,'|',2),11))
 		end) as px,
     coalesce(px_typ.target_concept,'OT')as px_type,
@@ -27,37 +26,56 @@ select distinct
 	coalesce(px_cd_1.vocabulary_id,px_cd_2.vocabulary_id,'OT') as raw_px_type,
 	procedure_type_concept_id as raw_ppx, 
 	'SITE' as site
-from SITE_pedsnet.procedure_occurrence po
-	join SITE_pcornet.encounter enc on cast(po.visit_occurrence_id as text)=enc.encounterid
-	left join vocabulary.concept px_cd_1 on px_cd_1.concept_id = po.procedure_concept_id and px_cd_1.vocabulary_id in ('HCPCS','CPT4','ICD10PCS','ICD9Proc')
-    left join vocabulary.concept px_cd_2 on px_cd_2.concept_id = po.procedure_source_concept_id and px_cd_2.vocabulary_id in ('HCPCS','CPT4','ICD10PCS','ICD9Proc')
-	left join pcornet_maps.pedsnet_pcornet_valueset_map px_typ 
- 		on case 
+from 
+	SITE_pedsnet.procedure_occurrence po
+inner join 
+	SITE_pcornet.person_visit_start2001 pvs 
+	on po.person_id = pvs.person_id
+	and po.visit_occurrence_id = pvs.visit_id
+join 
+	SITE_pcornet.encounter enc 
+	on cast(po.visit_occurrence_id as text)=enc.encounterid
+left join 
+	vocabulary.concept px_cd_1 
+	on px_cd_1.concept_id = po.procedure_concept_id 
+	and px_cd_1.vocabulary_id in ('HCPCS','CPT4','ICD10PCS','ICD9Proc')
+left join 
+	vocabulary.concept px_cd_2 
+	on px_cd_2.concept_id = po.procedure_source_concept_id 
+	and px_cd_2.vocabulary_id in ('HCPCS','CPT4','ICD10PCS','ICD9Proc')
+left join 
+	pcornet_maps.pedsnet_pcornet_valueset_map px_typ 
+ 	on 
+		case 
 			when px_cd_1.vocabulary_id is not null then px_cd_1.vocabulary_id
 	  		else px_cd_2.vocabulary_id 
 		end = px_typ.source_concept_id 
- 		and source_concept_class = 'Procedure Code Type'
-	left join pcornet_maps.pedsnet_pcornet_valueset_map m4 on cast(po.procedure_type_concept_id as text) = m4.source_concept_id AND
-	                                                                m4.source_concept_class='px source'
-	left join pcornet_maps.pedsnet_pcornet_valueset_map m5 on cast(po.procedure_type_concept_id as text) = m5.source_concept_id AND
-	                                                                m5.source_concept_class='ppx'                                                                
-	where  person_id IN (select person_id from SITE_pcornet.person_visit_start2001)
-	       and EXTRACT(YEAR FROM procedure_date) >= 2001
-	       and visit_occurrence_id is not null
-	       and visit_occurrence_id not in (select visit_occurrence_id from SITE_pedsnet.visit_occurrence where
-					extract(year from visit_start_date)<2001)
-	       and po.procedure_concept_id not in (select concept_id from vocabulary.concept where concept_class_id in ('CPT4 Modifier','2-dig nonbill code'))
-	       and po.procedure_source_concept_id not in (select concept_id from vocabulary.concept where concept_class_id in ('CPT4 Modifier','2-dig nonbill code'));
-
+ 	and source_concept_class = 'Procedure Code Type'
+left join 
+	pcornet_maps.pedsnet_pcornet_valueset_map m4 
+	on cast(po.procedure_type_concept_id as text) = m4.source_concept_id 
+	AND m4.source_concept_class='px source'
+left join 
+	pcornet_maps.pedsnet_pcornet_valueset_map m5 
+	on cast(po.procedure_type_concept_id as text) = m5.source_concept_id 
+	AND m5.source_concept_class='ppx'                                                                
+where 
+	EXTRACT(YEAR FROM procedure_date) >= 2001
+	and visit_occurrence_id is not null
+	and po.procedure_concept_id not in (select concept_id from vocabulary.concept where concept_class_id in ('CPT4 Modifier','2-dig nonbill code'))
+	and po.procedure_source_concept_id not in (select concept_id from vocabulary.concept where concept_class_id in ('CPT4 Modifier','2-dig nonbill code'))
+;
+commit;
 
 -- insert covid 19 immunization records into procedures table
+begin;
 insert into SITE_pcornet.procedures(
             proceduresid,patid, encounterid, enc_type, admit_date, providerid, px_date,px, px_type, px_source,
             ppx, raw_ppx,
             raw_px, raw_px_type,site)
 select
 	distinct on (immunization_id)('i'||immunization_id)::text as proceduresid,
-	cast(person_id as text) as patid,
+	cast(imm.person_id as text) as patid,
 	cast(visit_occurrence_id as text) as encounterid,
 	enc.enc_type as enc_type,
 	enc.admit_date as admit_date,
@@ -67,7 +85,7 @@ select
 		when immunization_concept_id=724907 then 91300 -- PFIZER
 		when immunization_concept_id=724906 then 91301  -- MODERNA
 		when immunization_concept_id=702866 then 91303 -- JANSSEN
-		end as px,
+	end as px,
     'CH' as px_type,
 	'DR' as px_source,
 	'OT' as ppx,
@@ -75,17 +93,22 @@ select
 	'CVX' as raw_px_type,
 	immunization_type_concept_id as raw_ppx, 
 	'SITE' as site
-from SITE_pedsnet.immunization imm
-	inner join SITE_pcornet.encounter enc on cast(imm.visit_occurrence_id as text)=enc.encounterid                                                           
-	where  
-		immunization_concept_id in (
+from 
+	SITE_pedsnet.immunization imm
+inner join 
+	SITE_pcornet.person_visit_start2001 pvs 
+	on imm.person_id = pvs.person_id
+	and imm.visit_occurrence_id = pvs.visit_id  
+join 
+	SITE_pcornet.encounter enc
+	on cast(imm.visit_occurrence_id as text)=enc.encounterid                                                        
+where  
+	immunization_concept_id in 
+		(
 		724907, -- PFIZER
 		724906, -- MODERNA
 		702866 -- JANSSEN
 		)
-		and person_id IN (select person_id from SITE_pcornet.person_visit_start2001)
-	       and EXTRACT(YEAR FROM immunization_date) >= 2001
-	       and (visit_occurrence_id not in (select visit_occurrence_id from SITE_pedsnet.visit_occurrence where
-					extract(year from visit_start_date)<2001) or visit_occurrence_id is null);
-
+	and EXTRACT(YEAR FROM immunization_date) >= 2001
+;
 commit;
