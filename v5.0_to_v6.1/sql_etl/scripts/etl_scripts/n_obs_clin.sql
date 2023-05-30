ecb0c4d764fdd8397d7eb175660cc272d6b734ec
Sam Boss <bossa@chop.edu>
2023-05-30 15:17:39 -0400

    comment out vital duplicate data

--- v5.0_to_v6.1/sql_etl/scripts/etl_scripts/n_obs_clin.sql
+++ v5.0_to_v6.1/sql_etl/scripts/etl_scripts/n_obs_clin.sql
@@ -15,52 +15,52 @@ from SITE_pcornet.obs_vaping;
 commit;
 
 /* vital data - ht, wt, systolic, diastolic, bmi, bp*/
-begin;
-INSERT INTO SITE_pcornet.obs_clin(encounterid, obsclin_code, obsclin_abn_ind, obsclin_start_date, obsclin_providerid, obsclin_result_modifier, obsclin_result_snomed, obsclin_result_qual, obsclin_result_text, 
-	obsclin_result_unit, obsclin_source, obsclin_start_time, obsclin_type, obsclinid, patid, raw_obsclin_code, raw_obsclin_modifier, raw_obsclin_name, raw_obsclin_result, raw_obsclin_type, 
-	raw_obsclin_unit, obsclin_stop_date, obsclin_stop_time,site)
-select distinct on (obsclinid) ms.visit_occurrence_id::int as encounterid, 
-case
-	when measurement_type_concept_id = 44818704 
-	then birth_meas.target_concept 
-	else coalesce(code.concept_code, null) 
-end as obsclin_code, 
-coalesce(abn.target_concept, 'NI') as obsclin_abn_ind,
-ms.measurement_date as obsclin_start_date, 
-ms.provider_id as obsclin_providerid, 
-coalesce(modif.target_concept,'NI') as obsclin_result_modifier, 
-null as obsclin_result_snomed, 
-coalesce(qual.target_concept,qual_src.target_concept,'NI') as obsclin_result_qual, 
-ms.value_as_number::text as obsclin_result_text, 
-coalesce(unit.target_concept, null) as obsclin_result_unit, 
-'HC' as obsclin_source,
-LPAD(date_part('hour',ms.measurement_datetime)::text,2,'0')||':'||LPAD(date_part('minute',ms.measurement_datetime)::text,2,'0') as obsclin_start_time,
-'LC' as obsclin_type, 
-('m'||measurement_id)::text as obsclinid, 
-person_id::text as patid, 
-coalesce(code.concept_code,'NI')  as raw_obsclin_code, 
-null as raw_obsclin_modifier, 
-case 
-	when measurement_type_concept_id = 44818704 
-	then birth_meas.concept_description 
-	else code.concept_name 
-end as raw_obsclin_name, 
-ms.value_as_number::text as raw_obsclin_result, 
-code.vocabulary_id as raw_obsclin_type, 
-ms.unit_source_value as raw_obsclin_unit, 
-null::date as obsclin_stop_date, 
-null as obsclin_stop_time,
-ms.site as site
-from SITE_pcornet.ms
-left join vocabulary.concept code on code.concept_id = ms.measurement_concept_id and code.vocabulary_id = 'LOINC'
-left join pcornet_maps.pedsnet_pcornet_valueset_map birth_meas on birth_meas.source_concept_id=ms.measurement_concept_id::text and birth_meas.source_concept_class = 'Birth Measures'
-left join pcornet_maps.pedsnet_pcornet_valueset_map modif on modif.source_concept_id = ms.operator_concept_id::text and modif.source_concept_class = 'Result modifier'
-left join pcornet_maps.pedsnet_pcornet_valueset_map unit on unit.source_concept_id = ms.unit_concept_id::text and unit.source_concept_class in ('Dose unit','Result unit')
-Left join pcornet_maps.pedsnet_pcornet_valueset_map abn on abn.source_concept_id::int = ms.value_as_concept_id and abn.source_concept_class = 'abnormal_indicator'
-left join pcornet_maps.pedsnet_pcornet_valueset_map qual on cast(ms.value_as_concept_id as text)= qual.source_concept_id and qual.source_concept_class = 'Result qualifier'
-left join pcornet_maps.pedsnet_pcornet_valueset_map qual_src on lower(ms.value_source_value) ilike '%'|| qual_src.concept_description || '%' and qual_src.source_concept_class = 'result_qual_source';
+-- begin;
+-- INSERT INTO SITE_pcornet.obs_clin(encounterid, obsclin_code, obsclin_abn_ind, obsclin_start_date, obsclin_providerid, obsclin_result_modifier, obsclin_result_snomed, obsclin_result_qual, obsclin_result_text, 
+-- 	obsclin_result_unit, obsclin_source, obsclin_start_time, obsclin_type, obsclinid, patid, raw_obsclin_code, raw_obsclin_modifier, raw_obsclin_name, raw_obsclin_result, raw_obsclin_type, 
+-- 	raw_obsclin_unit, obsclin_stop_date, obsclin_stop_time,site)
+-- select distinct on (obsclinid) ms.visit_occurrence_id::int as encounterid, 
+-- case
+-- 	when measurement_type_concept_id = 44818704 
+-- 	then birth_meas.target_concept 
+-- 	else coalesce(code.concept_code, null) 
+-- end as obsclin_code, 
+-- coalesce(abn.target_concept, 'NI') as obsclin_abn_ind,
+-- ms.measurement_date as obsclin_start_date, 
+-- ms.provider_id as obsclin_providerid, 
+-- coalesce(modif.target_concept,'NI') as obsclin_result_modifier, 
+-- null as obsclin_result_snomed, 
+-- coalesce(qual.target_concept,qual_src.target_concept,'NI') as obsclin_result_qual, 
+-- ms.value_as_number::text as obsclin_result_text, 
+-- coalesce(unit.target_concept, null) as obsclin_result_unit, 
+-- 'HC' as obsclin_source,
+-- LPAD(date_part('hour',ms.measurement_datetime)::text,2,'0')||':'||LPAD(date_part('minute',ms.measurement_datetime)::text,2,'0') as obsclin_start_time,
+-- 'LC' as obsclin_type, 
+-- ('m'||measurement_id)::text as obsclinid, 
+-- person_id::text as patid, 
+-- coalesce(code.concept_code,'NI')  as raw_obsclin_code, 
+-- null as raw_obsclin_modifier, 
+-- case 
+-- 	when measurement_type_concept_id = 44818704 
+-- 	then birth_meas.concept_description 
+-- 	else code.concept_name 
+-- end as raw_obsclin_name, 
+-- ms.value_as_number::text as raw_obsclin_result, 
+-- code.vocabulary_id as raw_obsclin_type, 
+-- ms.unit_source_value as raw_obsclin_unit, 
+-- null::date as obsclin_stop_date, 
+-- null as obsclin_stop_time,
+-- ms.site as site
+-- from SITE_pcornet.ms
+-- left join vocabulary.concept code on code.concept_id = ms.measurement_concept_id and code.vocabulary_id = 'LOINC'
+-- left join pcornet_maps.pedsnet_pcornet_valueset_map birth_meas on birth_meas.source_concept_id=ms.measurement_concept_id::text and birth_meas.source_concept_class = 'Birth Measures'
+-- left join pcornet_maps.pedsnet_pcornet_valueset_map modif on modif.source_concept_id = ms.operator_concept_id::text and modif.source_concept_class = 'Result modifier'
+-- left join pcornet_maps.pedsnet_pcornet_valueset_map unit on unit.source_concept_id = ms.unit_concept_id::text and unit.source_concept_class in ('Dose unit','Result unit')
+-- Left join pcornet_maps.pedsnet_pcornet_valueset_map abn on abn.source_concept_id::int = ms.value_as_concept_id and abn.source_concept_class = 'abnormal_indicator'
+-- left join pcornet_maps.pedsnet_pcornet_valueset_map qual on cast(ms.value_as_concept_id as text)= qual.source_concept_id and qual.source_concept_class = 'Result qualifier'
+-- left join pcornet_maps.pedsnet_pcornet_valueset_map qual_src on lower(ms.value_source_value) ilike '%'|| qual_src.concept_description || '%' and qual_src.source_concept_class = 'result_qual_source';
 
-commit;
+-- commit;
 
 /* vital data - tobacco*/
 begin;
