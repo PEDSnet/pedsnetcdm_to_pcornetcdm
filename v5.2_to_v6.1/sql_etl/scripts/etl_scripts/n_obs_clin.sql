8b4cf382a5163c782cce8ae770ad8c69b9338704
Hague <hagues@reslnpedsndb06.research.chop.edu>
2023-11-24 10:53:56 -0500

    remove tobacco

--- v5.2_to_v6.1/sql_etl/scripts/etl_scripts/n_obs_clin.sql
+++ v5.2_to_v6.1/sql_etl/scripts/etl_scripts/n_obs_clin.sql
@@ -99,16 +99,16 @@ commit;
 
 -- commit;
 
-begin;
-INSERT INTO SITE_pcornet.obs_clin( obsclinid,encounterid, obsclin_code, obsclin_abn_ind,obsclin_start_date, obsclin_providerid, obsclin_result_modifier, obsclin_result_snomed, obsclin_result_qual, obsclin_result_text, 
-	obsclin_result_unit, obsclin_source, obsclin_start_time, obsclin_type, patid, raw_obsclin_code, raw_obsclin_modifier, raw_obsclin_name, raw_obsclin_result, raw_obsclin_type, 
-	raw_obsclin_unit, obsclin_stop_date, obsclin_stop_time,site)
-select distinct on (obsclinid) obsclinid, encounterid, obsclin_code, obsclin_abn_ind,obsclin_start_date, obsclin_providerid, obsclin_result_modifier, obsclin_result_snomed, obsclin_result_qual, obsclin_result_text, 
-	obsclin_result_unit, obsclin_source, obsclin_start_time, obsclin_type, patid, raw_obsclin_code, raw_obsclin_modifier, raw_obsclin_name, raw_obsclin_result, raw_obsclin_type, 
-	raw_obsclin_unit, obsclin_stop_date, obsclin_stop_time,site
-from SITE_pcornet.tobacco_obclin;
+-- begin;
+--mINSERT INTO SITE_pcornet.obs_clin( obsclinid,encounterid, obsclin_code, obsclin_abn_ind,obsclin_start_date, obsclin_providerid, obsclin_result_modifier, obsclin_result_snomed, obsclin_result_qual, obsclin_result_text, 
+--	obsclin_result_unit, obsclin_source, obsclin_start_time, obsclin_type, patid, raw_obsclin_code, raw_obsclin_modifier, raw_obsclin_name, raw_obsclin_result, raw_obsclin_type, 
+--	raw_obsclin_unit, obsclin_stop_date, obsclin_stop_time,site)
+-- select distinct on (obsclinid) obsclinid, encounterid, obsclin_code, obsclin_abn_ind,obsclin_start_date, obsclin_providerid, obsclin_result_modifier, obsclin_result_snomed, obsclin_result_qual, obsclin_result_text, 
+--	obsclin_result_unit, obsclin_source, obsclin_start_time, obsclin_type, patid, raw_obsclin_code, raw_obsclin_modifier, raw_obsclin_name, raw_obsclin_result, raw_obsclin_type, 
+--	raw_obsclin_unit, obsclin_stop_date, obsclin_stop_time,site
+--from SITE_pcornet.tobacco_obclin;
 
-commit;
+--commit;
 
 begin;
 
@@ -173,6 +173,6 @@ begin;
 
 drop table if exists SITE_pcornet.meas_obsclin;
 drop table if exists SITE_pcornet.obs_vaping;
-drop table if exists SITE_pcornet.tobacco_obclin;
+-- drop table if exists SITE_pcornet.tobacco_obclin;
 drop table if exists SITE_pcornet.gestational_age;
 commit;
